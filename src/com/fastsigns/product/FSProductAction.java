38dfc30eacc7f807fa453597d05c18825af4ed69
YetAnotherMask <edamschroder@siliconmtn.com>
2014-03-27 15:31:07 -0400

    Fix bug where wrong method was being called in video creation

--- src/com/fastsigns/product/FSProductAction.java
+++ src/com/fastsigns/product/FSProductAction.java
@@ -456,7 +456,7 @@ public class FSProductAction extends SBActionAdapter {
 		while (rs.next()) {
 			video = new VideoVO(rs);
 			video.setAspectRatioHeight(Integer.parseInt(StringUtil.checkVal(rs.getString("ATTRIB1_TXT"))));
-			video.setAspectRatioHeight(Integer.parseInt(StringUtil.checkVal(rs.getString("ATTRIB2_TXT"))));
+			video.setAspectRatioWidth(Integer.parseInt(StringUtil.checkVal(rs.getString("ATTRIB2_TXT"))));
 			video.setAttribute("position", StringUtil.checkVal(rs.getString("VALUE_TXT")));
 			
 			product.addProdAttribute(rs.getString("product_attribute_id"), video);
