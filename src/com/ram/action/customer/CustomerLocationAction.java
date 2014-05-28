5ea6445f3be1beb198498ce140f78b9d1135ccb8
davesmt <dave@siliconmtn.com>
2014-05-28 15:40:30 -0400

    Adding classes for RAM user and customer management. WIP.

---
+++ src/com/ram/action/customer/CustomerLocationAction.java
@@ -0,0 +1,235 @@
+package com.ram.action.customer;
+
+// JDK 7
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+// RAMDataFeed
+import com.ram.datafeed.data.CustomerLocationVO;
+
+// SMTBaseLibs 2.0
+import com.siliconmtn.action.ActionException;
+import com.siliconmtn.action.ActionInitVO;
+import com.siliconmtn.gis.AbstractGeocoder;
+import com.siliconmtn.gis.GeocodeFactory;
+import com.siliconmtn.gis.GeocodeLocation;
+import com.siliconmtn.gis.GeocodeType;
+import com.siliconmtn.gis.Location;
+import com.siliconmtn.gis.MatchCode;
+import com.siliconmtn.http.SMTServletRequest;
+import com.siliconmtn.util.Convert;
+
+// WebCrescendo 2.0
+import com.smt.sitebuilder.action.SBActionAdapter;
+import com.smt.sitebuilder.common.ModuleVO;
+import com.smt.sitebuilder.common.PageVO;
+import com.smt.sitebuilder.common.constants.Constants;
+
+/****************************************************************************
+ * <b>Title: </b>CustomerLocationAction.java <p/>
+ * <b>Project: </b>WC_Custom <p/>
+ * <b>Description: </b>
+ * </p>
+ * <b>Copyright: </b>Copyright (c) 2014<p/>
+ * <b>Company: </b>Silicon Mountain Technologies<p/>
+ * @author David Bargerhuff
+ * @version 1.0<p/>
+ * @since May 20, 2014<p/>
+ *<b>Changes: </b>
+ * May 20, 2014: David Bargerhuff: Created class.
+ ****************************************************************************/
+public class CustomerLocationAction extends SBActionAdapter {
+	
+	/**
+	 * 
+	 */
+	public CustomerLocationAction() {
+		super(new ActionInitVO());
+	}
+
+	/**
+	 * @param actionInit
+	 */
+	public CustomerLocationAction(ActionInitVO actionInit) {
+		super(actionInit);
+	}
+	
+
+	
+	/* (non-Javadoc)
+	 * @see com.smt.sitebuilder.action.SBActionAdapter#list(com.siliconmtn.http.SMTServletRequest)
+	 */
+	@Override
+	public void retrieve(SMTServletRequest req) throws ActionException {
+		log.debug("CustomerLocationAction retrieve...");
+		int customerId = Convert.formatInteger(req.getParameter("customerId"), 0);
+		int customerLocationId = Convert.formatInteger(req.getParameter("customerLocationId"), 0);
+		String schema = (String)getAttribute("customDbSchema");
+		StringBuilder sql = new StringBuilder();
+		sql.append("select b.* from ").append(schema);
+		sql.append("RAM_CUSTOMER_LOCATION b where 1=1 ");
+		if (customerId > 0) sql.append("and CUSTOMER_ID = ? ");
+		if (customerLocationId > 0) sql.append("and CUSTOMER_LOCATION_ID = ? ");
+		sql.append("order by CUSTOMER_ID, LOCATION_NM");
+		log.debug("CustomerLocation retrieve SQL: " + sql.toString() + " | " + customerId + " | " + customerLocationId);
+		
+		PreparedStatement ps = null;
+		List<CustomerLocationVO> data = new ArrayList<>();
+		int index = 1;
+		try {
+			ps = dbConn.prepareStatement(sql.toString());
+			
+			if (customerId > 0) ps.setInt(index++, customerId);
+			if (customerLocationId > 0) ps.setInt(index++, customerLocationId);
+			
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				data.add(new CustomerLocationVO(rs, false));
+			}
+		} catch (SQLException e) {
+			log.error("Error retrieving RAM customer data, ", e);
+		} finally {
+			if (ps != null) {
+				try { 	ps.close(); }
+				catch (Exception e) { log.error("Error closing PreparedStatement, ", e); }
+			}
+		}
+		
+		ModuleVO modVo = (ModuleVO) attributes.get(Constants.MODULE_DATA);
+        modVo.setDataSize(data.size());
+        modVo.setActionData(data);
+        this.setAttribute(Constants.MODULE_DATA, modVo);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.smt.sitebuilder.action.SBActionAdapter#update(com.siliconmtn.http.SMTServletRequest)
+	 */
+	@Override
+	public void build(SMTServletRequest req) throws ActionException {
+		log.debug("CustomerLocationAction build...");
+		// instantiate a vo using the values on the request.
+		CustomerLocationVO vo = new CustomerLocationVO(req);
+		// geocode the location
+		getGeocode(vo);
+		
+		boolean isUpdate = (vo.getCustomerLocationId() > 0);
+		String msgAction = null;
+		String schema = (String)getAttribute("customDbSchema");
+		StringBuilder sql = new StringBuilder();
+		if (isUpdate) {
+			sql.append("update ").append(schema).append("RAM_CUSTOMER_LOCATION ");
+			sql.append("set REGION_ID=?, LOCATION_NM=?, ADDRESS_TXT=?, ADDRESS2_TXT=?, ");
+			sql.append("CITY_NM=?, STATE_CD=?, COUNTRY_CD=?, LATITUDE_NO=?, LONGITUDE=?, ");
+			sql.append("MATCH_CD=?, STOCKING_LOCATION_TXT=?, ACTIVE_FLG=?, UPDATE_DT=?, ");
+			sql.append("CUSTOMER_ID=? WHERE CUSTOMER_LOCATION_ID = ?");
+			msgAction = "updated";
+		} else {
+			// is an insert; Note: CUSTOMER_LOCATION_ID is an auto-incrementing field.
+			sql.append("insert into ").append(schema).append("RAM_CUSTOMER_LOCATION ");
+			sql.append("(REGION_ID, LOCATION_NM, ADDRESS_TXT, ADDRESS2_TXT, CITY_NM, ");
+			sql.append("STATE_CD, COUNTRY_CD, LATITUDE_NO, LONGITUDE_NO, MATCH_CD, ");
+			sql.append("STOCKING_LOCATION_TXT, ACTIVE_FLG, CREATE_DT, CUSTOMER_ID) ");
+			sql.append("VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			msgAction = "inserted";			
+		}
+		log.debug("CustomerLocation build SQL: " + sql.toString() + "|" + vo.getCustomerLocationId());
+		Object msg = null;
+		PreparedStatement ps = null;
+		try {
+			ps = dbConn.prepareStatement(sql.toString());
+			int index = 1;
+			// handles insert/update
+			ps.setString(index++, vo.getRegionId());
+			ps.setString(index++, vo.getLocationName());
+			ps.setString(index++, vo.getAddress());
+			ps.setString(index++, vo.getAddress2());
+			ps.setString(index++, vo.getCity());
+			ps.setString(index++, vo.getState());
+			ps.setString(index++, vo.getCountry());
+			ps.setDouble(index++, vo.getLatitude());
+			ps.setDouble(index++, vo.getLongitude());
+			ps.setString(index++, vo.getMatchCode().name());
+			ps.setString(index++, vo.getStockingLocation());
+			ps.setInt(index++, vo.getActiveFlag());
+			ps.setTimestamp(index++, Convert.getCurrentTimestamp());
+			ps.setInt(index++, vo.getCustomerId());
+			if (isUpdate) {
+				ps.setInt(index++, vo.getCustomerLocationId());
+			}
+			
+			ps.executeUpdate();
+			msg = "You have successfully " + msgAction + " the customer location record.";
+		} catch (SQLException sqle) {
+			log.error("Error managing RAM customer location record, ", sqle);
+			msg = sqle.getMessage();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (Exception e) {log.error("Error closing PrepraredStatement, ", e);}
+			}
+		}
+		
+        // Build the redirect and messages
+		// Setup the redirect.
+		StringBuilder url = new StringBuilder();
+		PageVO page = (PageVO) req.getAttribute(Constants.PAGE_DATA);
+		url.append(page.getRequestURI());
+		if (msg != null) url.append("?msg=").append(msg);
+		
+		log.debug("CustomerLocationAction redir: " + url);
+		req.setAttribute(Constants.REDIRECT_REQUEST, Boolean.TRUE);
+		req.setAttribute(Constants.REDIRECT_URL, url.toString());
+	}
+	
+	/**
+	 * Attempts to obtain a geocode for the referenced CustomerLocationVO passed in on the argument.  If the initial
+	 * matchcode of the CustomerLocationVO is 'state' or 'country', no geocode attempt is perfomed.  Otherwise geocode
+	 * retrieval is delegated to the SMTGeocoder service.  After geocoding has been performed, the latitude and longitude of the
+	 * CustomerLocationVO is updated unless the returned matchcode is one of 'county, 'state', 'country', or 'noMatch'.
+	 * 
+	 * @param cLoc
+	 */
+	public void getGeocode(CustomerLocationVO cLoc) {
+		log.debug("getting geocode...");
+		AbstractGeocoder ag = GeocodeFactory.getInstance((String)attributes.get(Constants.GEOCODE_CLASS));
+		ag.addAttribute(AbstractGeocoder.CONNECT_URL, attributes.get(Constants.GEOCODE_URL));
+		ag.addAttribute(AbstractGeocoder.CASS_VALIDATE_FLG, Boolean.FALSE);
+		
+		Location loc = null;
+		loc = new Location();
+		loc.setAddress(cLoc.getAddress());
+		loc.setCity(cLoc.getCity());
+		loc.setState(cLoc.getState());
+		loc.setZipCode(cLoc.getZipCode());
+		loc.setCountry(cLoc.getCountry());
+		
+		log.debug("Location info before geocoding: " + loc);
+		
+		if (loc.getGeocodeType().equals(GeocodeType.country) || loc.getGeocodeType().equals(GeocodeType.state)) {
+			//gl = new GeocodeLocation(loc.getFormattedLocation());
+			cLoc.setMatchCode(MatchCode.country);
+			if (loc.getGeocodeType().equals(GeocodeType.state)) cLoc.setMatchCode(MatchCode.state);
+		} else {
+			GeocodeLocation gl = ag.geocodeLocation(loc).get(0);
+			cLoc.setMatchCode(gl.getMatchCode());
+			log.debug("GeocodeLocation info after geocoding: " + gl);
+			// Set latitude/longitude depending upon the match code
+			switch (gl.getMatchCode()) {
+				case noMatch:
+				case country:
+				case state:
+				case county:
+					break;
+				default:
+					cLoc.setLatitude(gl.getLatitude());
+					cLoc.setLongitude(gl.getLongitude());
+					break;
+			}
+		}
+	}
+
+}
