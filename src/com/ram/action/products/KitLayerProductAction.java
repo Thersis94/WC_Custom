9fa49d943c1e4b466df86cdbc9f6ba3a7de5757b
billysmt <billy@siliconmtn.com?>
2014-06-20 17:07:20 -0400

    Updated code to properly save and retrieve Kit Products.

--- src/com/ram/action/products/KitLayerProductAction.java
+++ src/com/ram/action/products/KitLayerProductAction.java
@@ -4,13 +4,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.ram.datafeed.data.KitLayerProductVO;
 import com.ram.datafeed.data.LayerCoordinateVO;
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.http.SMTServletRequest;
+import com.siliconmtn.util.Convert;
 import com.smt.sitebuilder.action.SBActionAdapter;
 import com.smt.sitebuilder.common.constants.Constants;
 
@@ -62,7 +65,7 @@ public class KitLayerProductAction extends SBActionAdapter {
 		//Build Query
 		StringBuilder sb = new StringBuilder();
 		sb.append("select * from ").append(customDb).append("RAM_PRODUCT_LAYER_XR a ");
-		sb.append("inner join ").append(customDb).append("RAM_LAYER_COORDINATE b ");
+		sb.append("left outer join ").append(customDb).append("RAM_LAYER_COORDINATE b ");
 		sb.append("on a.PRODUCT_KIT_ID = b.PRODUCT_KIT_ID ");
 		sb.append("inner join ").append(customDb).append("RAM_PRODUCT c ");
 		sb.append("on a.product_id = c.product_id ");
@@ -98,6 +101,80 @@ public class KitLayerProductAction extends SBActionAdapter {
 	 */
 	@Override
 	public void build(SMTServletRequest req) throws ActionException {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("success", "true");
+		result.put("msg", "Data Successfully Updated");
+		String customDb = (String) attributes.get(Constants.CUSTOM_DB_SCHEMA);
+		String [] values = null;
+		List<KitLayerProductVO> inserts = new ArrayList<KitLayerProductVO>();
+		List<KitLayerProductVO> updates = new ArrayList<KitLayerProductVO>();
+		KitLayerProductVO vo = null;
+		StringBuilder insert = new StringBuilder();
+		insert.append("insert into ").append(customDb).append("RAM_PRODUCT_LAYER_XR (PRODUCT_ID, ");
+		insert.append("KIT_LAYER_ID, COORDINATE_TYPE_CD, CREATE_DT, ACTIVE_FLG) ");
+		insert.append("values (?,?,?,?,?)");
+		
+		StringBuilder update = new StringBuilder();
+		update.append("update ").append(customDb).append("RAM_PRODUCT_LAYER_XR set PRODUCT_ID = ?, ");
+		update.append("COORDINATE_TYPE_CD = ?, UPDATE_DT = ?, ");
+		update.append("ACTIVE_FLG = ? where PRODUCT_KIT_ID = ?");
+		
+		for(String s : req.getParameterMap().keySet())
+			if(s.startsWith("kitProduct_")) {
+				values = req.getParameter(s).split("\\|");
+				vo = new KitLayerProductVO();
+				vo.setProductKitId(Convert.formatInteger(values[0]));
+				vo.setProductId(Convert.formatInteger(values[1]));
+				vo.setKitLayerId(Convert.formatInteger(values[2]));
+				vo.setCoordinateType(values[3]);
+				vo.setActiveFlag(Convert.formatInteger(values[4]));
+				
+				if(!(vo.getProductId() > 0)) {
+					//Ignore these ones.
+				}
+				else if(vo.getProductKitId() > 0)
+					updates.add(vo);
+				else
+					inserts.add(vo);	
+			}
+		PreparedStatement ps = null;
+		try {
+			//Insert new Records
+			log.debug(insert);
+			ps = dbConn.prepareStatement(insert.toString());
+			for(KitLayerProductVO v : inserts) {
+				ps.setInt(1, v.getProductId());
+				ps.setInt(2, v.getKitLayerId());
+				ps.setString(3, v.getCoordinateType().name());
+				ps.setTimestamp(4, Convert.getCurrentTimestamp());
+				ps.setInt(5, v.getActiveFlag());
+				ps.addBatch();
+			}
+			ps.executeBatch();
+			ps.close();
+			
+			//Update Existing Records
+			log.debug(update.toString());
+			ps = dbConn.prepareStatement(update.toString());
+			for(KitLayerProductVO v : updates) {
+				ps.setInt(1, v.getProductId());
+				ps.setString(2, v.getCoordinateType().name());
+				ps.setTimestamp(3, Convert.getCurrentTimestamp());
+				ps.setInt(4, v.getActiveFlag());
+				ps.setInt(5, v.getProductKitId());
+				ps.addBatch();
+			}
+			ps.executeBatch();
+		} catch(SQLException sqle) {
+			log.error("Problem inserting/updating Kit Layer Products.", sqle);
+			result.put("success", "false");
+			result.put("msg", "Problem Saving Record");
+		} finally {
+			try {
+				ps.close();
+			} catch(Exception e){}
+		}
+		//super.putModuleData(result);
 	}
 
 	/* (non-Javadoc)
