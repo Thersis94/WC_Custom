3438abfcccf4db3d81561410ea13662f3d4ef534
YetAnotherMask <edamschroder@siliconmtn.com>
2015-03-10 18:06:56 -0400

    Finish first draft of data tool actions and bring variable and class names for beans more in line with rest of project.

--- src/com/depuysynthes/ifu/IFUAction.java
+++ src/com/depuysynthes/ifu/IFUAction.java
@@ -1,10 +1,20 @@
 package com.depuysynthes.ifu;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.db.pool.SMTDBConnection;
 import com.siliconmtn.http.SMTServletRequest;
+import com.siliconmtn.util.Convert;
+import com.siliconmtn.util.StringUtil;
+import com.siliconmtn.util.UUIDGenerator;
 import com.smt.sitebuilder.action.SBActionAdapter;
+import com.smt.sitebuilder.common.constants.Constants;
 
 /****************************************************************************
  * <b>Title</b>: IFUAction.java <p/>
@@ -35,52 +45,127 @@ public class IFUAction  extends SBActionAdapter {
 	}
 
 	public void retrieve(SMTServletRequest req) throws ActionException {
-		// Get the IFU instance id from the request object
+		String ifuId = req.getParameter("ifuId");
+		String sql = createRetrieveSql();
 		
-		// build the retrieve query to get the current ifu instance from
-		// the database as well as all technique guides that belong to it
+		IFUVO con = null;
+		try (PreparedStatement ps = dbConn.prepareStatement(sql)) {
+			ps.setString(1, ifuId);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				if (con == null) {
+					con = new IFUVO(rs);
+				}
+				String docTitle = rs.getString("IMPL_TITLE_TXT");
+				IFUDocumentVO doc = new IFUDocumentVO(rs);
+				doc.setTitle(docTitle);
+				
+				con.addIfuDocument(docTitle, doc);
+			}
+		} catch (SQLException e) {
+			log.error("Unable to get data for document: " + ifuId, e);
+		}
 		
-		// Run the query and build the document with a list of technique guides.
-		
-		// put the document on the request object.
+		super.putModuleData(con);
 	}
 	
+	private String createRetrieveSql() {
+		String customDb = (String) getAttribute(Constants.CUSTOM_DB_SCHEMA);
+		
+		StringBuilder sql = new StringBuilder();
+		
+		sql.append("SELECT *, dii.TITLE_TXT as IMPL_TITLE_TXT FROM ").append(customDb).append("DEPUY_IFU di ");
+		sql.append("LEFT JOIN ").append(customDb).append("DEPUY_IFU_IMPL dii on ");
+		sql.append("di.DEPUY_IFU_ID = dii.DEPUY_IFU_ID ");
+		sql.append("WHERE di.DEPUY_IFU_ID = ? ");
+		
+		return sql.toString();
+	}
+
 	public void list(SMTServletRequest req) throws ActionException {
-		// Get the IFU instance id from the request object
+		String customDb = (String) getAttribute(Constants.CUSTOM_DB_SCHEMA);
 		
-		// build the retrieve query to get the current ifu container as well
-		// as all of its instances
+		StringBuilder sql = new StringBuilder(55);
+		sql.append("SELECT * FROM ").append(customDb).append("DEPUY_IFU");
 		
-		// Run the query and build the container with a map of instances.
+		List<IFUVO> data = new ArrayList<IFUVO>();
+		try (PreparedStatement ps = dbConn.prepareStatement(sql.toString())) {
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				data.add(new IFUVO(rs));
+			}
+		} catch (SQLException e) {
+			log.error("Could not get list of IFU containers.", e);
+		}
 		
-		// put the container on the request object.
+		super.putModuleData(data);
 	}
 	
 	public void delete(SMTServletRequest req) throws ActionException {
-		// Get the current instance's id
+		String customDb = (String) getAttribute(Constants.CUSTOM_DB_SCHEMA);
+		String ifuId = req.getParameter("ifuId");
 		
-		// Build the delete query.  Approval is handled higher up so we don't need to do any extra checks here
+		StringBuilder sql = new StringBuilder(70);
+		sql.append("DELETE ").append(customDb).append("DEPUY_IFU WHERE DEPUY_IFU_ID = ?");
 		
-		// Execute the query and return
+		try (PreparedStatement ps = dbConn.prepareStatement(sql.toString())) {
+			ps.setString(1, ifuId);
+			
+			if (ps.executeUpdate() < 1)
+				log.warn("No records deleted for ifu: " + ifuId);
+		} catch (SQLException e) {
+			log.error("Unable to delete ifu with id: " + ifuId);
+		}
 	}
 
 	public void update(SMTServletRequest req) throws ActionException {
-		this.update(new IFUContainer(req));
+		this.update(new IFUVO(req));
 	}
 	
-	public void update(IFUContainer con) throws ActionException {
+	public void update(IFUVO vo) throws ActionException {
+		boolean isInsert = false;
+		if (StringUtil.checkVal(vo.getIfuId()).length() == 0) {
+			isInsert = true;
+			vo.setIfuId(new UUIDGenerator().getUUID());
+			vo.setIfuGroupId(vo.getIfuId());
+		}
 		
-		// build the update query with the build query function
+		String sql = buildUpdateSql(isInsert);
 		
-		// Set the values on the query with information on the vo
-		
-		//Execute the query and return
-		
+		try (PreparedStatement ps = dbConn.prepareStatement(sql)) {
+			int i = 1;
+			ps.setString(i++, vo.getIfuGroupId());
+			ps.setString(i++, vo.getTitle());
+			ps.setInt(i++, vo.getArchiveFlg());
+			ps.setInt(i++, vo.getOrderNo());
+			ps.setString(i++, vo.getVersion());
+			ps.setTimestamp(i++, Convert.getCurrentTimestamp());
+			ps.setString(i++, vo.getIfuId());
+			
+			if (ps.executeUpdate() > 1) 
+				log.warn("Nothing updated for IFU with id: " + vo.getIfuId());
+		} catch (SQLException e) {
+			log.error("Unable to update IFU with id: " + vo.getIfuId());
+		}
 	}
 	
-	private String buildUpdateSql() {
-		//Build the update sql for
-		return "";
+	private String buildUpdateSql(boolean isInsert) {
+		String customDb = (String) getAttribute(Constants.CUSTOM_DB_SCHEMA);
+		StringBuilder sql = new StringBuilder();
+		if (isInsert) {
+			sql.append("INSERT INTO ").append(customDb).append("DEPUY_IFU (");
+			sql.append("DEPUY_IFU_GROUP_ID, TITLE_TXT, ARCHIVED_FLG, ORDER_NO, VERSION_TXT, CREATE_DT, DEPUY_IFU_ID) ");
+			sql.append("VALUES(?,?,?,?,?,?,?)");
+		} else {
+			sql.append("UPDATE ").append(customDb).append("DEPUY_IFU SET ");
+			sql.append("DEPUY_IFU_GROUP_ID = ?, TITLE_TXT = ?, ARCHIVED_FLG = ?, ");
+			sql.append("ORDER_NO = ?, VERSION_TXT = ?, CREATE_DT = ? WHERE DEPUY_IFU_ID = ?) ");
+		}
+		
+		return sql.toString();
 	}
 
 }
--- src/com/depuysynthes/ifu/IFUDocumentVO.java
+++ src/com/depuysynthes/ifu/IFUDocumentVO.java
@@ -26,78 +26,69 @@ import com.siliconmtn.http.SMTServletRequest;
 public class IFUDocumentVO extends IFUVO implements Serializable{
 	  private static final long serialVersionUID = -8409489653118817155L;
 	
-	private String documentId;
-	private String ifuId;
-	private String url;
-	private String mediaBinId;
+	private String implId;
+	private String urlTxt;
+	private String dpySynMediaBinId;
 	private String languageCd;
 	private String atricleTxt;
 	private String partNoTxt;
 	private String defaultMsgTxt;
-	private List<TechniqueGuideVO> tgList;
+	private List<IFUTechniqueGuideVO> tgList;
 	
 	public IFUDocumentVO() {
-		setTgList(new ArrayList<TechniqueGuideVO>());
+		setTgList(new ArrayList<IFUTechniqueGuideVO>());
 	}
 
 	public IFUDocumentVO(SMTServletRequest req) {
 		super(req);
-		this.setDocumentId(req.getParameter("documentId"));
-		this.setDocumentId(req.getParameter("ifuId"));
-		this.setUrl(req.getParameter("url"));
-		this.setMediaBinId(req.getParameter("mediaBinId"));
+		this.setimplId(req.getParameter("implId"));
+		this.setimplId(req.getParameter("ifuId"));
+		this.setUrlTxt(req.getParameter("urlTxt"));
+		this.setDpySynMediaBinId(req.getParameter("dpySynMediaBinId"));
 		this.setLanguageCd(req.getParameter("languageCd"));
 		this.setAtricleTxt(req.getParameter("articleTxt"));
 		this.setPartNoTxt(req.getParameter("partNo"));
 		this.setDefaultMsgTxt(req.getParameter("defaultMsgTxt"));
-		setTgList(new ArrayList<TechniqueGuideVO>());
+		setTgList(new ArrayList<IFUTechniqueGuideVO>());
 	}
 
 	public IFUDocumentVO(ResultSet rs) {
 		super(rs);
 		DBUtil db = new DBUtil();
-		this.setDocumentId(db.getStringVal("DEPUY_IFU_IMPL_ID", rs));
+		this.setimplId(db.getStringVal("DEPUY_IFU_IMPL_ID", rs));
 		this.setIfuId(db.getStringVal("DEPUY_IFU_ID", rs));
-		this.setUrl(db.getStringVal("URL_TXT", rs));
-		this.setMediaBinId(db.getStringVal("DPY_SYN_MEDIABIN_ID", rs));
+		this.setUrlTxt(db.getStringVal("URL_TXT", rs));
+		this.setDpySynMediaBinId(db.getStringVal("DPY_SYN_MEDIABIN_ID", rs));
 		this.setLanguageCd(db.getStringVal("LANGUAGE_CD", rs));
 		this.setAtricleTxt(db.getStringVal("ARTICLE_TXT", rs));
 		this.setPartNoTxt(db.getStringVal("PART_NO_TXT", rs));
 		this.setDefaultMsgTxt(db.getStringVal("DEFAULT_MSG_TXT", rs));
 		db = null;
-		setTgList(new ArrayList<TechniqueGuideVO>());
+		setTgList(new ArrayList<IFUTechniqueGuideVO>());
 	}
 
-	public String getDocumentId() {
-		return documentId;
+	public String getImplId() {
+		return implId;
 	}
 
-	public void setDocumentId(String documentId) {
-		this.documentId = documentId;
+	public void setimplId(String implId) {
+		this.implId = implId;
 	}
 
-	public String getIfuId() {
-		return ifuId;
+	public String getUrlTxt() {
+		return urlTxt;
 	}
 
-	public void setIfuId(String ifuId) {
-		this.ifuId = ifuId;
+	public void setUrlTxt(String urlTxt) {
+		this.urlTxt = urlTxt;
 	}
 
-	public String getUrl() {
-		return url;
+	public String getDpySynMediaBinId() {
+		return dpySynMediaBinId;
 	}
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getMediaBinId() {
-		return mediaBinId;
-	}
-
-	public void setMediaBinId(String mediaBinId) {
-		this.mediaBinId = mediaBinId;
+	public void setDpySynMediaBinId(String dpySynMediaBinId) {
+		this.dpySynMediaBinId = dpySynMediaBinId;
 	}
 
 	public String getLanguageCd() {
@@ -132,15 +123,15 @@ public class IFUDocumentVO extends IFUVO implements Serializable{
 		this.defaultMsgTxt = defaultMsgTxt;
 	}
 
-	public List<TechniqueGuideVO> getTgList() {
+	public List<IFUTechniqueGuideVO> getTgList() {
 		return tgList;
 	}
 
-	public void setTgList(List<TechniqueGuideVO> tgList) {
+	public void setTgList(List<IFUTechniqueGuideVO> tgList) {
 		this.tgList = tgList;
 	}
 	
-	public void addTg(TechniqueGuideVO vo) {
+	public void addTg(IFUTechniqueGuideVO vo) {
 		tgList.add(vo);
 	}
 
