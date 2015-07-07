f97dc1133867474903b4b058ffeaca2459c5c213
Erik <wingo@siliconmtn.com>
2015-07-07 12:03:39 -0600

    Implemented list method, used call to BlogAction to fetch the actionId from the blog group id.

--- src/com/fastsigns/action/franchise/FranchiseBlogAction.java
+++ src/com/fastsigns/action/franchise/FranchiseBlogAction.java
@@ -9,13 +9,14 @@ import com.fastsigns.action.franchise.vo.FranchiseVO;
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.http.SMTServletRequest;
+import com.siliconmtn.util.Convert;
 import com.siliconmtn.util.StringUtil;
 import com.smt.sitebuilder.action.SBActionAdapter;
 import com.smt.sitebuilder.action.SBModuleVO;
+import com.smt.sitebuilder.action.blog.BlogAction;
 import com.smt.sitebuilder.action.blog.BlogFacadeAction;
 import com.smt.sitebuilder.action.blog.BlogGroupVO;
 import com.smt.sitebuilder.common.ModuleVO;
-import com.smt.sitebuilder.common.constants.AdminConstants;
 import com.smt.sitebuilder.common.constants.Constants;
 
 /****************************************************************************
@@ -43,6 +44,15 @@ public class FranchiseBlogAction extends SBActionAdapter {
 	public FranchiseBlogAction(ActionInitVO arg0) {
 		super(arg0);
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.smt.sitebuilder.action.SBActionAdapter#list(com.siliconmtn.http.SMTServletRequest)
+	 */
+	@Override
+	public void list(SMTServletRequest req) throws ActionException{
+		super.retrieve(req);
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -53,20 +63,20 @@ public class FranchiseBlogAction extends SBActionAdapter {
 		req.setParameter(SB_ACTION_ID, actionInit.getActionId());
 		super.retrieve(req);
 		
-		//if this is a request for a list we don't need the rest of the data (SBActionAdapter.list sometimes calls retrieve)
-		String reqType = StringUtil.checkVal(req.getParameter(AdminConstants.REQUEST_TYPE));
-		if (reqType.equals(AdminConstants.REQ_LIST)){
-			return;
-		}
-		
 		ModuleVO mod = (ModuleVO) this.getAttribute(Constants.MODULE_DATA);
 		SBModuleVO data = (SBModuleVO) mod.getActionData();
 		
 		FranchiseBlogVO vo = new FranchiseBlogVO();
 		//get franchise location data
 		this.getFranchise(req, vo);
-		//get the group of blogs to display (actionId stored in attrib1Text)
-		this.getBlogGroup(req,vo, (String)data.getAttribute(SBModuleVO.ATTRIBUTE_1));
+		
+		String gId = (String)data.getAttribute(SBModuleVO.ATTRIBUTE_1); 
+		boolean isPreview = Convert.formatBoolean(req.getAttribute(Constants.PAGE_PREVIEW), false);
+		//get the group of blogs to display (blogGroupId stored in attrib1Text)
+		BlogAction ba = new BlogAction(actionInit);
+		ba.setAttributes(attributes);
+		ba.setDBConnection(dbConn);
+		this.getBlogGroup(req,vo, ba.getBlogActionId(gId, isPreview));
 		
 		this.putModuleData(vo);
 	}
