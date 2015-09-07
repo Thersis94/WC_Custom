13f62b89dc9148488c286ec3d622800a2aa7cfce
YetAnotherMask <edamschroder@siliconmtn.com>
2015-09-07 11:08:57 -0400

    Initial commit for NeXus phase 2.

--- src/com/depuysynthes/nexus/NexusProductVO.java
+++ src/com/depuysynthes/nexus/NexusProductVO.java
@@ -1,9 +1,12 @@
 package com.depuysynthes.nexus;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.siliconmtn.annotations.SolrField;
+import com.siliconmtn.db.DBUtil;
 import com.smt.sitebuilder.util.solr.SolrDocumentVO;
 
 /****************************************************************************
@@ -19,9 +22,10 @@ import com.smt.sitebuilder.util.solr.SolrDocumentVO;
  * @updates:
  ****************************************************************************/
 
-public class NexusProductVO extends  SolrDocumentVO {
+public class NexusProductVO extends SolrDocumentVO {
 	
 	public final static String solrIndex = "DEPUY_NEXUS";
+	private String productId;
 	private String orgId;
 	private String orgName ;
 	private List<String> gtin;
@@ -34,6 +38,10 @@ public class NexusProductVO extends  SolrDocumentVO {
 	private List<String> uomLevel;
 	private String region;
 	private String status;
+	private int orderNo;
+	private boolean dateLot;
+	private Date start;
+	private Date end;
 
 	
 	public NexusProductVO() {
@@ -48,7 +56,54 @@ public class NexusProductVO extends  SolrDocumentVO {
 		this();
 		setProductName(name);
 	}
+	
+	public NexusProductVO(ResultSet rs) {
+		this();
+		setData(rs);
+	}
+	
+	public void setData(ResultSet rs) {
+		DBUtil db = new DBUtil();
+		productId = db.getStringVal("PRODUCT_SKU_TXT", rs);
+		quantity = db.getIntVal("QUANTITY_NO", rs);
+		gtin.add(db.getStringVal("ITEM_GTIN_TXT", rs));
+		primaryDeviceId = db.getStringVal("ITEM_GTIN_NO", rs);
+		start = db.getDateVal("EFFECTIVE_START_DT", rs);
+		end = db.getDateVal("EFFECTIVE_END_DT", rs);
+		uomLevel.add(db.getStringVal("UNIT_MEASURE_CD", rs));
+		orderNo = db.getIntVal("ORDER_NO", rs);
+	}
+	
+	public NexusProductVO clone() throws CloneNotSupportedException {
+		NexusProductVO clone =  (NexusProductVO) super.clone();
+		clone.setProductId(productId);
+		clone.setOrgId(orgId);
+		clone.setOrgName(orgName);
+		clone.setGtin(new ArrayList<String>());
+		for (String s : gtin) clone.addGtin(s);
+		clone.setGtinLevel(new ArrayList<String>());
+		for (String s : gtinLevel) clone.addGtinLevel(s);
+		clone.setPrimaryDeviceId(primaryDeviceId);
+		clone.setDpmGTIN(dpmGTIN);
+		clone.setQuantity(quantity);
+		clone.setPackageLevel(new ArrayList<String>());
+		for (String s : packageLevel) clone.addPackageLevel(s);
+		clone.setUomLevel(new ArrayList<String>());
+		for (String s : uomLevel) clone.addUOMLevel(s);
+		clone.setRegion(region);
+		clone.setStatus(status);
+		clone.setEnd(end);
+		clone.setStart(start);
+		return clone;
+	}
 
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
 
 	public String getProductName() {
 		return getDocumentId();
@@ -206,6 +261,14 @@ public class NexusProductVO extends  SolrDocumentVO {
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
 
 	/**
 	 * Gathers all relevant information that could be used for autocompletion
@@ -219,6 +282,22 @@ public class NexusProductVO extends  SolrDocumentVO {
 		for (String s : gtin) auto.append(s).append(" ");
 		auto.append(getSummary());
 		return auto.toString();
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
 	}
 
 }
