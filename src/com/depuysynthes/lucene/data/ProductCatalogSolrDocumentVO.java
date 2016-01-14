3384f1954f93d70effaecc2f4ef5c9f120dacb44
YetAnotherMask <edamschroder@siliconmtn.com>
2016-01-14 09:32:45 -0500

    Remove unneeded product attribute map.

--- src/com/depuysynthes/lucene/data/ProductCatalogSolrDocumentVO.java
+++ src/com/depuysynthes/lucene/data/ProductCatalogSolrDocumentVO.java
@@ -1,9 +1,6 @@
 package com.depuysynthes.lucene.data;
 
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 
@@ -33,7 +30,6 @@ public class ProductCatalogSolrDocumentVO extends SolrDocumentVO {
 	private String thumbImage = null;
 	private int dsOrderNo;
 	private String specialty = null;
-	private Map<String, List<String>> prodAttributes = null;
 
 	/**
 	 * 
@@ -41,7 +37,6 @@ public class ProductCatalogSolrDocumentVO extends SolrDocumentVO {
 	public ProductCatalogSolrDocumentVO() {
 		this(ProductCatalogSolrIndex.INDEX_TYPE);
 		super.setUpdateDt(Calendar.getInstance().getTime());
-		prodAttributes = new HashMap<>();
 	}
 
 	/**
@@ -49,7 +44,6 @@ public class ProductCatalogSolrDocumentVO extends SolrDocumentVO {
 	 */
 	public ProductCatalogSolrDocumentVO(String solrIndex) {
 		super(solrIndex);
-		prodAttributes = new HashMap<>();
 	}
 
 	/**
@@ -103,18 +97,5 @@ public class ProductCatalogSolrDocumentVO extends SolrDocumentVO {
 	@SolrField(name="opco_ss")
 	public String getSpecialty(){
 		return specialty;
-	}
-
-	public void setProdAttributes(Map<String, List<String>> prodAttributes) {
-		this.prodAttributes = prodAttributes;
-	}
-
-	@SolrField(name="attributes")
-	public Map<String, List<String>> getProdAttributes(){
-		return prodAttributes;
-	}
-	
-	public void addProdAttribute(String key, List<String> value) {
-		prodAttributes.put(key, value);
 	}
 }
