df74a4a80ed3ae8c338ab0af50bfb9115472b135
billysmt <billy@siliconmtn.com>
2017-01-18 17:25:42 -0500

    Added GapAnalysis Action to Controller

--- src/com/biomed/smarttrak/action/AdminControllerAction.java
+++ src/com/biomed/smarttrak/action/AdminControllerAction.java
@@ -3,6 +3,7 @@
  */
 package com.biomed.smarttrak.action;
 
+import com.biomed.smarttrak.action.gap.GapFacadeAction;
 import com.biomed.smarttrak.admin.ContentHierarchyAction;
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
@@ -81,6 +82,9 @@ public class AdminControllerAction extends SimpleActionAdapter {
 		switch (StringUtil.checkVal(actionType)) {
 			case "contentHierarchy":
 				action = new ContentHierarchyAction();
+				break;
+			case "gapAnalysis":
+				action = new GapFacadeAction();
 				break;
 			default:
 				throw new ActionException("Action type not supported.");
