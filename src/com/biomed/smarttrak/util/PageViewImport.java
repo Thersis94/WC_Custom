e62c3bc6b1fbcb31106596d6e684c14c0d29ad65
Dave <dave@siliconmtn.com>
2017-01-25 14:47:19 -0500

    smarttrak-task-FE-3: Updated and tested page view importer.

--- src/com/biomed/smarttrak/util/PageViewImport.java
+++ src/com/biomed/smarttrak/util/PageViewImport.java
@@ -10,17 +10,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
+
+// Log4j
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 // SMTBaseLibs
 import com.siliconmtn.db.DatabaseConnection;
 import com.siliconmtn.io.mail.EmailMessageVO;
 import com.siliconmtn.util.CommandLineUtil;
 import com.siliconmtn.util.Convert;
+import com.siliconmtn.util.UUIDGenerator;
 
 // WebCresendo libs
 import com.smt.sitebuilder.util.PageViewVO;
@@ -39,12 +42,19 @@ import com.smt.sitebuilder.util.PageViewVO;
  ***************************************************************************/
 public class PageViewImport extends CommandLineUtil {
 
-	private int lastRecordRead;
-	private Timestamp importTimestamp;
+	private String importTimestamp;
+	private String lastTimestampProcessed;
 	private Connection destDbConn;
+	private Logger log = Logger.getLogger(PageViewImport.class);
+	private final String PROP_WC_INSTANCE_CD = "wcInstanceCode";
+	private final String PROP_TIMESTAMP_LOG_PATH = "timestampLogPath";
+	private String debugPropsPath = "/home/groot/Downloads/smarttrak/pageViewImport.properties";
+	private String debugLogPropsPath = "/home/groot/Downloads/smarttrak/log4j.properties";
 	
 	public PageViewImport(String[] args) {
 		super(args);
+		if (args.length == 0) 
+			PropertyConfigurator.configure(debugLogPropsPath);
 	}
 
 	public static void main(String[] args) {
@@ -61,31 +71,31 @@ public class PageViewImport extends CommandLineUtil {
 		try {
 			// 1. load properties or die
 			loadProperties();
-		
+
 			// 2. get source db conn or die
 			getSourceDbConnection();
-			
+
 			// 3. get destination db conn or die
 			getDestinationDbConnection();
-			
+
 			// 4. init the last record read value or die
 			//initLastRecordRead();
 			retrieveImportTimestamp();
-			
+
 			// 5. retrieve source records or die
 			pageViews = retrieveSourceRecords();
-			
+
 			// 6. process source records
 			processSourceRecords(pageViews);
-			
+
 			// 7. persist the timestamp
-			writeNewImportTimestamp();
-			
+			if (! pageViews.isEmpty()) persistImportTimestamp();
+
 		} catch (Exception e) {
-			log.error(e.getMessage());
+			log.error("Error during import, full stacktrace: " + e);
 			notifyAdmin(e);
 		} 
-		
+
 		// 7. clean up.
 		closeConnections();
 	}
@@ -96,9 +106,14 @@ public class PageViewImport extends CommandLineUtil {
 	 * @throws IOException
 	 */
 	private void loadProperties() throws IOException {
-		super.loadProperties(args[0]);
+		if (args.length > 0) 	
+			super.loadProperties(args[0]);
+		else 
+			super.loadProperties(debugPropsPath);
+		// superclass eats exceptions, let's check for successful load.
 		if (props.isEmpty()) 
-			throw new IOException("Error: Properties file not loaded.");
+			throw new IOException("Error: Unable to load properties file.");
+		log.info("Loaded properties file.");
 	}
 	
 	/**
@@ -108,7 +123,8 @@ public class PageViewImport extends CommandLineUtil {
 	private void getSourceDbConnection() throws SQLException {
 		loadDBConnection(props);
 		if (dbConn == null) 
-			throw new SQLException("Error: unable to obtain source db connection.");
+			throw new SQLException("Error: Unable to obtain source db connection.");
+		log.info("Obtained source db connection.");
 	}
 	
 	/**
@@ -117,7 +133,7 @@ public class PageViewImport extends CommandLineUtil {
 	 */
 	protected void getDestinationDbConnection() 	throws SQLException {
 		if (props.get("destDbDriver") == null) {
-			throw new SQLException("Error: Destination db property values not found.");
+			throw new SQLException("Error: Unable to find destination db property values.");
 		}
 		
 		DatabaseConnection dbc = new DatabaseConnection();
@@ -130,32 +146,7 @@ public class PageViewImport extends CommandLineUtil {
 		} catch (Exception e) {
 			throw new SQLException("Error: " + e.getMessage());
 		}
-	}
-
-	/**
-	 * Retrieves the primary key value of the last record inserted into the 
-	 * destination db in order to use that record (plus one) as the starting
-	 * point for retrieving the next set of source records.
-	 * @throws SQLException
-	 */
-	@SuppressWarnings("unused")
-	private void initLastRecordRead() throws SQLException {
-		if (lastRecordRead > 0) return;
-		/* Read the max pkid from the destination db table to determine
-		 * the last record that we read from the source db table. */
-		StringBuilder sql = new StringBuilder(45);
-		sql.append("select max(page_view_id) from page_view_user");
-		try (PreparedStatement ps = destDbConn.prepareStatement(sql.toString())) {
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				lastRecordRead = rs.getInt("page_view_id");
-			} else {
-				throw new SQLException("Error: Unable to retrieve ID of last page view record inserted.");
-			}
-		} catch (SQLException sqle) {
-			log.error("Error obtaining ID of last destination table page view record.", sqle);
-			throw new SQLException(sqle.getMessage());
-		}
+		log.info("Obtained destination db connection.");
 	}
 	
 	/**
@@ -165,37 +156,75 @@ public class PageViewImport extends CommandLineUtil {
 	 */
 	private void retrieveImportTimestamp() throws IOException {
 		try {
-			Path tsPath = FileSystems.getDefault().getPath(props.getProperty("timestampLogPath"));
+			Path tsPath = FileSystems.getDefault().getPath(props.getProperty(PROP_TIMESTAMP_LOG_PATH));
 			byte[] tsBytes = Files.readAllBytes(tsPath);
-			String tsRaw = new String(tsBytes);
-			importTimestamp = Convert.formatTimestamp(Convert.DATE_TIME_DASH_PATTERN, tsRaw);
+			importTimestamp = new String(tsBytes);
 		} catch (IOException ioe) {
-			log.warn("Warning: Unable to retrieve previous page view timestamp, using 'now' minus 5 minutes." + ioe.getMessage());
+			log.warn("Warning: Unable to retrieve previous page view timestamp: " + ioe.getMessage());
 			Calendar cal = Calendar.getInstance();
 			cal.add(Calendar.MINUTE, -5);
-			importTimestamp = Convert.formatTimestamp(Convert.formatSQLDate(cal.getTime()));
+			importTimestamp = getTimeString(cal);
 		}
-		log.info("Using timestamp value of: " + importTimestamp);
+		log.info("Using import timestamp value of: " + importTimestamp);
+	}
+	
+	/**
+	 * Helper method to format time as a specific String
+	 * @param cal
+	 * @return
+	 */
+	private String getTimeString(Calendar cal) {
+		StringBuilder sb = new StringBuilder(24);
+		sb.append(cal.get(Calendar.YEAR));
+		sb.append("-");
+		int val = cal.get(Calendar.MONTH + 1);
+		padTime(sb,val,"-");
+		val = cal.get(Calendar.DAY_OF_MONTH);
+		padTime(sb,val," ");
+		val = cal.get(Calendar.HOUR_OF_DAY);
+		padTime(sb,val,":");
+		val = cal.get(Calendar.MINUTE);
+		padTime(sb,val,":");
+		val = cal.get(Calendar.SECOND);
+		padTime(sb,val,".");
+		val = cal.get(Calendar.MILLISECOND);
+		if (val < 100) sb.append("0");
+		padTime(sb,val,null);
+		return sb.toString();
+	}
+	
+	/**
+	 * Pads time values with a 0 and delimiter.
+	 * @param sb
+	 * @param val
+	 * @param delim
+	 */
+	private void padTime(StringBuilder sb, int val, String delim) {
+		if (val < 10) sb.append("0");
+		sb.append(val);
+		if (delim == null) return;
+		sb.append(delim);
 	}
 	
 	/**
 	 * Writes a new timestamp to 
 	 * @throws IOException
 	 */
-	private void writeNewImportTimestamp() throws IOException {
+	private void persistImportTimestamp() throws IOException {
 		try {
-			Path tsPath = FileSystems.getDefault().getPath(props.getProperty("timestampLogPath"));
-			byte[] tsBytes = importTimestamp.toString().getBytes();
+			Path tsPath = FileSystems.getDefault().getPath(props.getProperty(PROP_TIMESTAMP_LOG_PATH));
+			byte[] tsBytes = lastTimestampProcessed.toString().getBytes();
 			if (Files.isWritable(tsPath)) {
 				Files.deleteIfExists(tsPath);
 				Files.write(tsPath, tsBytes);
+				log.info("Persisted last timestamp found for next import start: " + lastTimestampProcessed);
 			} else {
-				throw new IOException("Error: Timestamp log path is not writable!, Path is: " + tsPath.toString());
+				throw new IOException("Error persisting timestamp, path is not writable: " + tsPath.toString());
 			}
 			
 		} catch (IOException ioe) {
-			log.error("Error writing next import timestamp, ", ioe);
-			throw new IOException("Error writing next import timestamp, check logs!");
+			log.error("Error persisting timestamp: ", ioe);
+			throw new IOException("Error persisting timestamp, check logs!");
 		}
 	}
 
@@ -208,28 +237,37 @@ public class PageViewImport extends CommandLineUtil {
 	private List<PageViewVO> retrieveSourceRecords() throws SQLException {
 		StringBuilder sql = new StringBuilder(120);
 		sql.append("select pageview_id, site_id, profile_id, session_id, page_id, request_uri_txt, ");
-		sql.append("query_str_txt, visit_dt from pageview ");
+		sql.append("query_str_txt, wc_instance_cd, visit_dt from pageview ");
 		sql.append("where wc_instance_cd = ? and visit_dt > ? order by visit_dt");
 		log.debug("Source page view SQL: " + sql.toString());
 		PageViewVO pvo = null;
 		List<PageViewVO> pageViews = new ArrayList<>();
 		try (PreparedStatement ps = dbConn.prepareStatement(sql.toString())) {
-			ps.setString(1, props.getProperty("WC_Instance_ID"));
-			ps.setTimestamp(2, Convert.formatTimestamp(new Date()));
-			ps.setInt(1, lastRecordRead);
+			ps.setString(1, props.getProperty(PROP_WC_INSTANCE_CD));
+			ps.setString(2, importTimestamp.toString());
 			ResultSet rs = ps.executeQuery();
+			//SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			//Date d;
 			while (rs.next()) {
 				pvo = new PageViewVO();
-				pvo.setPageViewId(rs.getInt("page_view_id"));
+				pvo.setPageViewId(rs.getInt("pageview_id"));
 				pvo.setSiteId(rs.getString("site_id"));
 				pvo.setProfileId(rs.getString("profile_id"));
-				pvo.setQueryString(rs.getString("querystring_uri"));
-				pvo.setVisitDate(rs.getDate("visit_dt"));
+				pvo.setSessionId(rs.getString("session_id"));
+				pvo.setPageId(rs.getString("page_id"));
+				pvo.setRequestUri(rs.getString("request_uri_txt"));
+				pvo.setQueryString(rs.getString("query_str_txt"));
+				pvo.setWcInstance(rs.getString("wc_instance_cd"));
+				lastTimestampProcessed = rs.getString("visit_dt");
+				pvo.setVisitDate(Convert.formatDate(lastTimestampProcessed));
 				pageViews.add(pvo);
 			}
 		} catch (SQLException sqle) {
-			throw new SQLException("Error: Unable to retrieve source page view records: " + sqle.getMessage());
+			log.error("Error retrieving source page view records, ", sqle);
+			throw new SQLException("Error: Unable to retrieve source page view records, " + sqle.getMessage());
 		}
+
+		log.debug("pageViews retrieved: " + pageViews.size());
 		return pageViews;
 	}
 	
@@ -242,20 +280,23 @@ public class PageViewImport extends CommandLineUtil {
 			throws SQLException {
 		if (pageViews.isEmpty()) return;
 		StringBuilder sql = new StringBuilder(200);
-		sql.append("insert into page_view_user (page_view_id, site_id, ");
-		sql.append("profile_id, session_id, page_id, query_string_uri, visit_dt) values (?,?,?,?,?");
+		sql.append("insert into pageview_user (pageview_user_id, site_id, profile_id, session_id, ");
+		sql.append("page_id, request_uri_txt, query_str_txt, wc_instance_cd, visit_dt) ");
+		sql.append("values (?,?,?,?,?,?,?,?,?)");
 		log.debug("Destination table insert SQL: " + sql.toString());
 		
 		try (PreparedStatement ps = destDbConn.prepareStatement(sql.toString())) {
 			int idx = 1;
 			int recCnt = 0;
 			for (PageViewVO vo : pageViews) {
-				ps.setInt(idx++, vo.getPageViewId());
+				ps.setString(idx++, new UUIDGenerator().getUUID());
 				ps.setString(idx++, vo.getSiteId());
 				ps.setString(idx++, vo.getProfileId());
 				ps.setString(idx++, vo.getSessionId());
 				ps.setString(idx++, vo.getPageId());
+				ps.setString(idx++, vo.getRequestUri());
 				ps.setString(idx++, vo.getQueryString());
+				ps.setString(idx++, vo.getWcInstance());
 				ps.setDate(idx++, Convert.formatSQLDate(vo.getVisitDate()));
 				ps.addBatch();
 				idx = 1;
@@ -265,11 +306,10 @@ public class PageViewImport extends CommandLineUtil {
 			// insert any remaining batched inserts.
 			if (recCnt % 100 > 0) ps.executeBatch();
 			
-			// capture the timestamp from the last vo in the list.
-			importTimestamp = Convert.formatTimestamp(pageViews.get(pageViews.size() -1).getVisitDate());
 		} catch (BatchUpdateException bue) {
 			throw new SQLException(bue.getNextException().getMessage());
 		}
+
 	}
 	
 	/**
@@ -285,7 +325,7 @@ public class PageViewImport extends CommandLineUtil {
 	}
 	
 	/**
-	 * Notifies admin of error
+	 * Notifies admin if an error ocurred during import.
 	 * @param exception
 	 */
 	private void notifyAdmin(Throwable exception) {
@@ -295,13 +335,16 @@ public class PageViewImport extends CommandLineUtil {
 		errMsg.append("|");
 		errMsg.append(exception.getMessage());
 		EmailMessageVO evo = new EmailMessageVO();
-		evo.setSubject("Error: PageView import failed!");
+		evo.setSubject("Pageview Importer: Error, import failed, check logs!");
+		evo.setHtmlBody(errMsg.toString());
+		evo.setTextBody(errMsg.toString());
 		try {
-			evo.setFrom(props.getProperty("adminFrom"));
-			evo.addRecipient(props.getProperty("adminTo"));
+			evo.setFrom(props.getProperty("fromAddress"));
+			evo.addRecipient(props.getProperty("toAddress"));
 		} catch (Exception e) {
-			log.error("Error formatting EmailMessageVO, ", e);
+			log.error("Error formatting message: ", e);
 			try {
+				// try 'last resort' email addresses.
 				evo.setFrom("appsupport@siliconmtn.com");
 				evo.addRecipient("dave@siliconmtn.com");
 			} catch (Exception ex) {}
