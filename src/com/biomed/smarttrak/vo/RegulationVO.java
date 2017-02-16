d17209dcb9f7ce9aed6c34b4a43717755d05cbff
YetAnotherMask <edamschroder@siliconmtn.com>
2017-02-16 11:09:39 -0500

    Add fields neccesary for the public side site.

--- src/com/biomed/smarttrak/vo/RegulationVO.java
+++ src/com/biomed/smarttrak/vo/RegulationVO.java
@@ -4,8 +4,11 @@
 package com.biomed.smarttrak.vo;
 
 import java.sql.ResultSet;
+import java.util.Date;
 
 import com.siliconmtn.db.DBUtil;
+import com.siliconmtn.db.orm.Column;
+import com.siliconmtn.db.orm.Table;
 
 /****************************************************************************
  * <b>Title</b>: RegulationVO.java
@@ -18,13 +21,17 @@ import com.siliconmtn.db.DBUtil;
  * @version 1.0
  * @since Feb 6, 2017
  ****************************************************************************/
+@Table(name="BIOMEDGPS_REGULATORY_PRODUCT")
 public class RegulationVO {
 
 	private String regulatorId;
 	private String regionId;
+	private String regionName;
 	private String pathId;
+	private String pathName;
 	private String productId;
 	private String statusId;
+	private String statusName;
 
 	public RegulationVO() {
 		super();
@@ -42,11 +49,15 @@ public class RegulationVO {
 		pathId = db.getStringVal("path_id", rs);
 		productId = db.getStringVal("product_id", rs);
 		statusId = db.getStringVal("status_id", rs);
+		regionName = db.getStringVal("region_nm", rs);
+		pathName = db.getStringVal("path_nm", rs);
+		statusName = db.getStringVal("status_nm", rs);
 	}
 
 	/**
 	 * @return the regulatorId
 	 */
+	@Column(name="regulatory_id", isPrimaryKey=true)
 	public String getRegulatorId() {
 		return regulatorId;
 	}
@@ -54,13 +65,20 @@ public class RegulationVO {
 	/**
 	 * @return the regionId
 	 */
+	@Column(name="region_id")
 	public String getRegionId() {
 		return regionId;
+	}
+
+	@Column(name="region_nm", isReadOnly=true)
+	public String getRegionName() {
+		return regionName;
 	}
 
 	/**
 	 * @return the pathId
 	 */
+	@Column(name="path_id")
 	public String getPathId() {
 		return pathId;
 	}
@@ -68,6 +86,7 @@ public class RegulationVO {
 	/**
 	 * @return the productId
 	 */
+	@Column(name="product_id")
 	public String getProductId() {
 		return productId;
 	}
@@ -75,9 +94,21 @@ public class RegulationVO {
 	/**
 	 * @return the statusId
 	 */
+	@Column(name="status_id")
 	public String getStatusId() {
 		return statusId;
 	}
+
+	@Column(name="path_nm", isReadOnly=true)
+	public String getPathName() {
+		return pathName;
+	}
+
+	@Column(name="status_nm", isReadOnly=true)
+	public String getStatusName() {
+		return statusName;
+	}
+
 
 	/**
 	 * @param regulatorId the regulatorId to set.
@@ -91,6 +122,10 @@ public class RegulationVO {
 	 */
 	public void setRegionId(String regionId) {
 		this.regionId = regionId;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
 	}
 
 	/**
@@ -113,4 +148,19 @@ public class RegulationVO {
 	public void setStatusId(String statusId) {
 		this.statusId = statusId;
 	}
+
+	public void setPathName(String pathName) {
+		this.pathName = pathName;
+	}
+	
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	
+
+	// These functions exists only to give the DBProcessor a hook to autogenerate dates on
+	@Column(name="UPDATE_DT", isAutoGen=true, isUpdateOnly=true)
+	public Date getUpdateDate() {return null;}
+	@Column(name="CREATE_DT", isAutoGen=true, isInsertOnly=true)
+	public Date getCreateDate() {return null;}
 }
