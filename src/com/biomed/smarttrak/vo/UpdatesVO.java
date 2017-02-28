9c5155171d68001fb654a6f7183865a477d684eb
billysmt <billy@siliconmtn.com>
2017-02-28 11:05:01 -0500

    Fixed DocId logic

--- src/com/biomed/smarttrak/vo/UpdatesVO.java
+++ src/com/biomed/smarttrak/vo/UpdatesVO.java
@@ -283,9 +283,11 @@ public class UpdatesVO extends SecureSolrDocumentVO implements HumanNameIntfc {
 	 */
 	public void setUpdateId(String updateId) {
 		this.updateId = updateId;
-		StringBuilder docId = new StringBuilder();
-		if (updateId != null && updateId.length() < AdminControllerAction.DOC_ID_MIN_LEN) {
-			docId.append(UpdateIndexer.INDEX_TYPE).append("_").append(updateId);
+		StringBuilder docId = new StringBuilder(updateId);
+		if (docId != null && docId.length() < AdminControllerAction.DOC_ID_MIN_LEN) {
+
+			//Insert separator and then insert Index Type
+			docId.insert(0, "_").insert(0, UpdateIndexer.INDEX_TYPE);
 		}
 		setDocumentId(docId.toString());
 	}
