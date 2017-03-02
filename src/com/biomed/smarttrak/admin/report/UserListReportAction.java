9b5ab66541281c0c9020ba7852f0f1b1e86ee446
Dave <dave@siliconmtn.com>
2017-03-02 17:05:35 -0500

    smarttrak-Q3-4: Adding UserList report.

--- src/com/biomed/smarttrak/admin/report/UserListReportAction.java
+++ src/com/biomed/smarttrak/admin/report/UserListReportAction.java
@@ -1,14 +1,31 @@
 package com.biomed.smarttrak.admin.report;
 
+// Java 8
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+// WC custom
+import com.biomed.smarttrak.vo.UserVO;
+
+// SMTBaseLibs
+import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
+import com.siliconmtn.security.PhoneVO;
+import com.siliconmtn.security.StringEncrypter;
+import com.siliconmtn.util.Convert;
+import com.siliconmtn.util.StringUtil;
+
+// WebCrescendo
 import com.smt.sitebuilder.action.SimpleActionAdapter;
+import com.smt.sitebuilder.common.SiteVO;
+import com.smt.sitebuilder.common.constants.Constants;
 
 /*****************************************************************************
  <p><b>Title</b>: UserListReportAction.java</p>
@@ -37,60 +54,320 @@ public class UserListReportAction extends SimpleActionAdapter {
 		super(arg0);
 	}
 	
-	public void retrieveUserList(ActionRequest req) {
+	/**
+	 * Retrieves the user list report data.
+	 * @param req
+	 * @return
+	 * @throws ActionException
+	 */
+	public List<AccountUsersVO> retrieveUserList(ActionRequest req) throws ActionException {
+		
+		StringEncrypter se = initStringEncrypter();
 		
 		// 1. retrieve account/users
-		List<AccountUsersVO> accounts = retrieveAccountUsers();
+		List<AccountUsersVO> accounts = retrieveAccountUsers(se);
 		
 		// 2. retrieve profiles, profile addresses, phones
+		Map<String,Date> lastLogins = retrieveLastLogin(req, accounts);
 		
-		// 3. retrieve registration data
+		// 3. Merge data and return.
+		mergeData(accounts, lastLogins);
 		
-		// 4. Mix together and bake @ 350 for 30ms.
-		
-		// 5. return data.
-		
+		return accounts;
 	}
-	
-	protected List<AccountUsersVO> retrieveAccountUsers() {
+
+	/**
+	 * Retrieves accounts and users.
+	 * @param se
+	 * @return
+	 */
+	protected List<AccountUsersVO> retrieveAccountUsers(StringEncrypter se) {
 		// 1. build query
 		StringBuilder sql = buildAccountsUsersQuery();
-		
+
 		// 2. build PS
 		try (PreparedStatement ps = dbConn.prepareStatement(sql.toString())) {
 			ResultSet rs = ps.executeQuery();
-			return parseAccountUsers(rs);
+
+			return parseAccountUsers(se,rs);
+
 		} catch (SQLException sqle) {
 			log.error("Error retrieving accounts and users, ",sqle);
 			return new ArrayList<>();
 		}
-		
 	}
 	
-	protected List<AccountUsersVO> parseAccountUsers(ResultSet rs) {
-		List<AccountUsersVO> accounts = new ArrayList<>();
+	/**
+	 * Retrieves accounts and users.
+	 * @param req
+	 * @param accounts
+	 * @return
+	 */
+	protected Map<String,Date> retrieveLastLogin(ActionRequest req, List<AccountUsersVO> accounts) {
 		
-		return accounts;
+		SiteVO site = (SiteVO)req.getAttribute(Constants.SITE_DATA);
+		String siteId = StringUtil.isEmpty(site.getAliasPathParentId()) ? site.getSiteId() : site.getAliasPathParentId();
+		// 1. build query
+		StringBuilder sql = buildLastLoginQuery();
+
+		// 2. build PS
+		Map<String,Date> lastLogins = new HashMap<>();
+		try (PreparedStatement ps = dbConn.prepareStatement(sql.toString())) {
+			ps.setString(1, siteId);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				lastLogins.put(rs.getString("authentication_id"), rs.getDate("login_dt"));
+			}
+
+		} catch (SQLException sqle) {
+			log.error("Error retrieving user last login dates, ",sqle);
+		}
+		
+		return lastLogins;
 	}
 	
+	/**
+	 * Merges last login data with account users.
+	 * @param accounts
+	 * @param lastLogins
+	 */
+	protected void mergeData(List<AccountUsersVO> accounts, Map<String,Date> lastLogins) {
+		for (AccountUsersVO account : accounts) {
+			List<UserVO> users = account.getUsers();
+			for (UserVO user : users) {
+				Date dt = lastLogins.get(user.getAuthenticationId());
+				if (dt == null) continue;
+				user.setLoginDate(dt);
+			}
+		}
+	}
+	
+	/**
+	 * Builds the last logins query.
+	 * @return
+	 */
+	protected StringBuilder buildLastLoginQuery() {
+		StringBuilder sql = new StringBuilder(425);
+		sql.append("select authentication_id, login_dt from ( ");
+		sql.append("select distinct(al.authentication_id), login_dt, ");
+		sql.append("rank() over ( partition by al.authentication_id order by login_dt desc ) ");
+		sql.append("from custom.biomedgps_user us ");
+		sql.append("inner join profile pf on us.profile_id = pf.profile_id ");
+		sql.append("inner join authentication_log al on pf.authentication_id = al.authentication_id ");
+		sql.append("where site_id = ? ) rank_filter where rank = 1 order by authentication_id ");
+		log.debug("last login retrieval SQL: " + sql.toString());
+		return sql;
+	}
+	
+	/**
+	 * Parses the last login query results.
+	 * @param rs
+	 * @return
+	 * @throws SQLException
+	 */
+	protected Map<String,Date> parseLastLogin(ResultSet rs) throws SQLException {
+		Map<String,Date> lastLogins = new HashMap<>();
+		while (rs.next()) {
+			lastLogins.put(rs.getString("authentication_id"), rs.getDate("login_dt"));
+		}
+		return lastLogins;
+	}
+
 	/**
 	 * Builds the base accounts/users query.
 	 * @return
 	 */
 	protected StringBuilder buildAccountsUsersQuery() {
-		StringBuilder sql = new StringBuilder(800);
-		sql.append("select ac.account_id, ac.account_nm, ");
-		sql.append("c.expiration_dt as acct_expire_dt, ");
-		sql.append("us.profile_id, us.user_id, us.status_cd, us.fd_auth_flg, ");
-		sql.append("us.expiration_dt as user_expire_dt, us.create_dt as join_dt, ");
+		StringBuilder sql = new StringBuilder(650);
+		sql.append("select ac.account_id, ac.account_nm, ac.expiration_dt as acct_expiration_dt, ");
+		sql.append("us.status_cd, us.expiration_dt, us.fd_auth_flg, us.create_dt, ");
+		sql.append("pf.profile_id, pf.authentication_id, pf.first_nm, pf.last_nm, pf.email_address_txt, ");
+		sql.append("pfa.address_txt, pfa.address2_txt, pfa.city_nm, pfa.state_cd, pfa.zip_cd, pfa.country_cd, ");
+		sql.append("ph.phone_number_txt, ph.phone_type_cd, ");
 		sql.append("rd.register_field_id, rd.value_txt ");
 		sql.append("from custom.biomedgps_account ac ");
 		sql.append("inner join custom.biomedgps_user us on ac.account_id = us.account_id ");
-		sql.append("inner join register_submittal rs on us.profile_id = rs.profile_id ");
+		sql.append("inner join profile pf on us.profile_id = pf.profile_id ");
+		sql.append("left join profile_address pfa on pf.profile_id = pfa.profile_id ");
+		sql.append("left join phone_number ph on pf.profile_id = ph.profile_id ");
+		sql.append("inner join register_submittal rs on pf.profile_id = rs.profile_id ");
 		sql.append("inner join register_data rd on rs.register_submittal_id = rd.register_submittal_id ");
-		sql.append("order by account_nm, profile_id ");
-		log.debug("accounts users SQL: " + sql.toString());
+		sql.append("order by ac.account_nm, us.profile_id, phone_type_cd");
+		log.debug("user retrieval SQL: " + sql.toString());
 		return sql;
 	}
 
+	/**
+	 * Parses the accounts & users query results.
+	 * @param rs
+	 * @return
+	 * @throws SQLException
+	 */
+	protected List<AccountUsersVO> parseAccountUsers(StringEncrypter se, 
+			ResultSet rs) throws SQLException {
+		
+		log.debug("parseAccountsUsers...");
+		String prevAcctId = null;
+		String prevPid = null;
+		String prevPhoneCd =  null;
+		String currAcctId;
+		String currPid;
+		String currPhoneCd;
+
+		UserVO user = new UserVO();
+		AccountUsersVO account = new AccountUsersVO();
+		List<AccountUsersVO> accounts = new ArrayList<>();
+
+		while (rs.next()) {
+
+			currAcctId = rs.getString("account_id");
+			currPid = rs.getString("profile_id");
+			currPhoneCd = StringUtil.checkVal(rs.getString("phone_type_cd"));
+
+			if (! currAcctId.equals(prevAcctId)) {
+				// acct changed, capture 'previous' user and account
+				if (prevAcctId != null) {
+					account.addUser(user);
+					accounts.add(account);
+				}
+
+				// now create new account, and new user
+				account = createBaseAccount(rs);
+				user = createBaseUser(se,rs);
+
+			} else {
+				// same account, check for user change
+				if (! currPid.equals(prevPid)) {
+					// user changed, capture 'previous' user
+					if (prevPid != null) {
+						account.addUser(user);
+					}
+
+					// now create new user
+					user = createBaseUser(se,rs);
+					
+				} else {
+					// same user, check to see if phone type changed.
+					checkUserPhoneType(se,user,rs.getString("phone_number_txt"),prevPhoneCd,currPhoneCd);
+					
+				}	
+
+				// add registration record
+				user.addAttribute(rs.getString("register_field_id"), rs.getString("value_txt"));
+
+			}
+
+			prevAcctId = currAcctId;
+			prevPid = currPid;
+			prevPhoneCd = currPhoneCd;
+		}
+
+		// pick up the dangler
+		if (prevAcctId != null) {
+			if (prevPid != null) { 
+				account.addUser(user);
+			}
+			accounts.add(account);
+		}
+
+		return accounts;
+	}
+
+	/**
+	 * Creates base AccountUsersVO.
+	 * @param rs
+	 * @return
+	 * @throws SQLException
+	 */
+	protected AccountUsersVO createBaseAccount(ResultSet rs) throws SQLException {
+		AccountUsersVO account = new AccountUsersVO();
+		account.setAccountId(rs.getString("account_id"));
+		account.setAccountName(rs.getString("account_nm"));
+		account.setExpirationDate(rs.getDate("acct_expiration_dt"));
+		return account;
+	}
+	
+	/**
+	 * Creates base UserVO
+	 * @param se
+	 * @param rs
+	 * @return
+	 * @throws SQLException
+	 */
+	protected UserVO createBaseUser(StringEncrypter se, ResultSet rs) throws SQLException {
+		UserVO user = new UserVO();
+		// set unencrypted fields
+		user.setAccountId(rs.getString("account_id"));
+		user.setProfileId(rs.getString("profile_id"));
+		user.setAuthenticationId(rs.getString("authentication_id"));
+		user.setStatusCode(rs.getString("status_cd"));
+		user.setFdAuthFlg(Convert.formatInteger(rs.getInt("fd_auth_flg")));
+		user.setExpirationDate(rs.getDate("expiration_dt"));
+		user.setCreateDate(rs.getDate("create_dt"));
+		user.setAddress2(rs.getString("address2_txt"));
+		user.setCity(rs.getString("city_nm"));
+		user.setState(rs.getString("state_cd"));
+		user.setZipCode(rs.getString("zip_cd"));
+		user.setCountryCode(rs.getString("country_cd"));
+		
+		// decrypt encrypted fields and set.
+		try {
+			user.setFirstName(se.decrypt(rs.getString("first_nm")));
+			user.setLastName(se.decrypt(rs.getString("last_nm")));
+			user.setEmailAddress(se.decrypt(rs.getString("email_address_txt")));
+			// check address txt, decrypt if populated.
+			String tmp = StringUtil.checkVal(rs.getString("address_txt"));
+			if (! tmp.isEmpty()) user.setAddress(se.decrypt(tmp));
+			tmp = StringUtil.checkVal(rs.getString("phone_number_txt"));
+			if (! tmp.isEmpty()) {
+				if (PhoneVO.HOME_PHONE.equals(rs.getString("phone_type_cd"))) {
+					user.setMainPhone(tmp);
+				} else {
+					user.setMobilePhone(tmp);
+				}
+			}
+		} catch (Exception e) {
+			log.warn("Warning: Unable to decrypt profile fields for profile ID " + user.getProfileId());
+		}
+		return user;
+	}
+	
+	/**
+	 * Checks for a phone type code change and adds phone to UserVO if necessary.
+	 * @param se
+	 * @param user
+	 * @param phoneText
+	 * @param prevPhoneCd
+	 * @param currPhoneCd
+	 */
+	protected void checkUserPhoneType(StringEncrypter se, UserVO user, 
+			String phoneText, String prevPhoneCd, String currPhoneCd) {
+		if (currPhoneCd.equals(prevPhoneCd) || currPhoneCd.isEmpty()) return;
+		try {
+			String tmp = se.decrypt(phoneText);
+			if (PhoneVO.HOME_PHONE.equals(currPhoneCd)) {
+				user.setMainPhone(tmp);
+			} else {
+				user.setMobilePhone(tmp);
+			}
+		} catch (Exception e) {
+			log.warn("Warning: Unable to decrypt user phone type.");
+		}
+	}
+	
+	/**
+	 * Instantiates a StringEncrypter.
+	 * @return
+	 * @throws ActionException
+	 */
+	protected StringEncrypter initStringEncrypter() throws ActionException {
+		StringEncrypter se = null;
+		try {
+			se = new StringEncrypter((String)attributes.get(Constants.ENCRYPT_KEY));
+		} catch (Exception e) {
+			throw new ActionException("Error instantiating StringEncrypter: " + e.getMessage());
+		}
+		return se;
+	}
+	
 }
---
+++ src/com/biomed/smarttrak/admin/report/UserListReportVO.java
@@ -0,0 +1,297 @@
+package com.biomed.smarttrak.admin.report;
+
+// Java 8
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+//WC custom
+import com.biomed.smarttrak.vo.UserVO;
+
+//SMTBaseLibs
+import com.siliconmtn.data.report.ExcelReport;
+import com.siliconmtn.util.Convert;
+import com.siliconmtn.util.PhoneNumberFormat;
+import com.siliconmtn.util.StringUtil;
+
+// WebCrescendo
+import com.smt.sitebuilder.action.AbstractSBReportVO;
+
+/*****************************************************************************
+ <p><b>Title</b>: UserListReportVO.java</p>
+ <p><b>Description: </b></p>
+ <p> 
+ <p>Copyright: (c) 2000 - 2017 SMT, All Rights Reserved</p>
+ <p>Company: Silicon Mountain Technologies</p>
+ @author DBargerhuff
+ @version 1.0
+ @since Mar 02, 2017
+ <b>Changes:</b> 
+ ***************************************************************************/
+public class UserListReportVO extends AbstractSBReportVO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7975115731097780002L;
+	
+	private List<AccountUsersVO> accounts;
+	private static final String REPORT_TITLE = "User List Export";
+	// account fields
+	private static final String ACCT_EXPIRE = "ACCT_EXPIRE";
+	private static final String ACCT_NM = "ACCT_NM";
+	
+	// profile fields
+	private static final String FIRST_NM = "FIRST_NM";
+	private static final String LAST_NM = "LAST_NM";
+	private static final String EMAIL = "EMAIL";
+	private static final String ADDRESS1 = "ADDRESS1";
+	private static final String ADDRESS2 = "ADDRESS2";
+	private static final String CITY_NM = "CITY_NM";
+	private static final String STATE_CD = "STATE_CD";
+	private static final String POSTAL_CD = "POSTAL_CD";
+	private static final String COUNTRY_CD = "COUNTRY_CD";
+	private static final String MAIN_PHONE = "PHONE";
+	private static final String MOBILE_PHONE = "MOBILE_PHONE";
+	
+	// SmartTRAK user fields
+	private static final String USER_STATUS = "USER_STATUS";
+	private static final String USER_EXPIRE = "USER_EXPIRE";
+	private static final String HAS_FD = "HAS_FD";
+	
+	// SmartTRAK user reg fields
+	private static final String COMPANY = "COMPANY";
+	private static final String COMPANY_URL = "COMPANY_URL";
+	private static final String DATE_DEMOED = "DATE_DEMOED";
+	private static final String DATE_JOINED = "DATE_JOINED";
+	private static final String DATE_TRAINED = "DATE_TRAINED";
+	private static final String DIVISION = "DIVISION";
+	private static final String FAV_UPDATES = "FAV_UPDATES";
+	private static final String INDUSTRY = "INDUSTRY";
+	private static final String JOB_CATEGORY = "JOB_CATEGORY";
+	private static final String JOB_LEVEL = "JOB_LEVEL";
+	private static final String NOTES = "NOTES";
+	private static final String SOURCE = "SOURCE";
+	private static final String TITLE = "TITLE";
+	private static final String UPDATES = "UPDATES";
+
+	// other fields
+	private static final String LAST_LOGIN = "LAST_LOGIN";
+	// TODO: add private static final String OS = "OS";
+	// TODO: add private static final String BROWSER = "BROWSER";
+	// TODO: add private static final String HITS = "HITS";
+
+	private static final String EMPTY_STRING = "";
+	private static final String LIST_DELIMITER = ",";
+	private static final String USER_FD_VAL = "FD";
+	private static final String LOGIN_DATE_NULL_VAL = "Never";
+	
+	/**
+	* Constructor
+	*/
+	public UserListReportVO() {
+        super();
+        setContentType("application/vnd.ms-excel");
+        isHeaderAttachment(Boolean.TRUE);
+        setFileName(REPORT_TITLE+".xls");
+        accounts = new ArrayList<>();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.siliconmtn.data.report.AbstractReport#generateReport()
+	 */
+	@Override
+	public byte[] generateReport() {
+		log.debug("generateReport...");
+
+		ExcelReport rpt = new ExcelReport(getHeader());
+		rpt.setTitleCell(REPORT_TITLE);
+
+		List<Map<String, Object>> rows = new ArrayList<>(accounts.size() * 5);
+		rows = generateDataRows(rows);
+
+		rpt.setData(rows);
+		return rpt.generateReport();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.siliconmtn.data.report.AbstractReport#setData(java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setData(Object o) {
+		this.accounts =  (List<AccountUsersVO>) o;
+	}
+	
+	/**
+	 * this method is used to generate the data rows of the excel sheet.
+	 * @param rows
+	 * @return
+	 */
+	private List<Map<String, Object>> generateDataRows(
+			List<Map<String, Object>> rows) {
+		PhoneNumberFormat pnf = new PhoneNumberFormat();
+		
+		// loop the account map
+		for (AccountUsersVO acct : accounts) {
+
+			// user vals
+			Map<String,Object> row;
+			
+			// loop account users
+			for (UserVO user : acct.getUsers()) {
+				row = new HashMap<>();
+				row.put(ACCT_NM, acct.getAccountName());
+				row.put(ACCT_EXPIRE, formatDate(acct.getExpirationDate()));
+				row.put(USER_EXPIRE, formatDate(user.getExpirationDate()));
+				row.put(COMPANY,user.getCompany());
+				row.put(TITLE,user.getTitle());
+				row.put(FIRST_NM,user.getFirstName());
+				row.put(LAST_NM,user.getLastName());
+				row.put(EMAIL,user.getEmailAddress());
+				row.put(LAST_LOGIN, formatDate(user.getLoginDate(),true));
+				// TODO: row.put(HITS,user.getHits());
+				row.put(MAIN_PHONE,formatPhoneNumber(pnf,user.getMainPhone(),user.getCountryCode()));
+				row.put(MOBILE_PHONE,formatPhoneNumber(pnf,user.getMobilePhone(),user.getCountryCode()));
+				row.put(ADDRESS1,user.getAddress());
+				row.put(ADDRESS2,user.getAddress2());
+				row.put(CITY_NM,user.getCity());
+				row.put(STATE_CD,user.getState());
+				row.put(POSTAL_CD,user.getZipCode());
+				row.put(COUNTRY_CD,user.getCountryCode());
+				row.put(USER_STATUS, user.getStatusCode());
+				row.put(SOURCE, user.getSource());
+				row.put(UPDATES, StringUtil.capitalize(user.getUpdates()));
+				row.put(FAV_UPDATES, StringUtil.capitalize(user.getFavoriteUpdates()));
+				row.put(DATE_JOINED, formatDate(user.getCreateDate()));
+				row.put(DIVISION, formatUserDivisions(user.getDivisions()));
+				// TODO: row.put(OS, user.getOs());
+				// TODO: row.put(BROWSER, user.getBrowser());
+				row.put(HAS_FD, formatUserFDFlag(user.getFdAuthFlg()));
+				row.put(NOTES, user.getNotes());
+				row.put(COMPANY_URL, user.getCompanyUrl());
+				row.put(JOB_CATEGORY, user.getJobCategory());
+				row.put(JOB_LEVEL, user.getJobLevel());
+				row.put(INDUSTRY, user.getIndustry());
+				row.put(DATE_DEMOED, user.getDemoDate());
+				row.put(DATE_TRAINED, user.getTrainingDate());
+				rows.add(row);
+			}
+		}
+		
+		return rows;
+	}
+	
+	/**
+	 * Formats a phone number based on the country
+	 * @param pnf
+	 * @param phone
+	 * @param country
+	 * @return
+	 */
+	protected String formatPhoneNumber(PhoneNumberFormat pnf, String phone, String country) {
+		if (StringUtil.checkVal(phone).isEmpty()) return EMPTY_STRING;
+		pnf.setCountryCode(country);
+		pnf.setPhoneNumber(phone);
+		return pnf.getFormattedNumber();
+		
+	}
+	
+	/**
+	 * Formats the date using Convert.DATE_SLASH_ABBREV_PATTERN (i.e. MMM/dd/yyyy)
+	 * @param date
+	 * @return
+	 */
+	protected String formatDate(Date date) {
+		return formatDate(date,false);
+	}
+	
+	/**
+	 * Formats the date using Convert.DATE_SLASH_ABBREV_PATTERN (i.e. MMM/dd/yyyy).
+	 * If isLastLogin is true and the date argument is null, the constant LOGIN_DATE_NULL_VAL
+	 * is returned.
+	 * @param date
+	 * @param isLastLogin
+	 * @return
+	 */
+	protected String formatDate(Date date, boolean isLastLogin) {
+		String dStr = null;
+		if (date == null) { 
+			if (isLastLogin) {
+				dStr = LOGIN_DATE_NULL_VAL;
+			}
+		} else {
+			dStr = Convert.formatDate(date,Convert.DATE_SLASH_ABBREV_PATTERN);
+		}
+		return dStr;
+	}
+	
+	/**
+	 * Parses user divisions list into a delimited String.
+	 * @param divisions
+	 * @return
+	 */
+	protected String formatUserDivisions(List<String> divisions) {
+		String divs = EMPTY_STRING;
+		if (divisions != null && ! divisions.isEmpty()) {
+			divs = StringUtil.getDelimitedList(divisions.toArray(new String[]{}), false, LIST_DELIMITER);
+		}
+		return divs;
+	}
+	
+	/**
+	 * Formats the value displayed for the user's FD field.
+	 * @param fd
+	 * @return
+	 */
+	protected String formatUserFDFlag(int fd) {
+		return fd == 1 ? USER_FD_VAL : EMPTY_STRING;
+	}
+
+	/**
+	 * builds the header map for the excel report
+	 * @return
+	 */
+	protected HashMap<String, String> getHeader() {
+		HashMap<String, String> headerMap = new LinkedHashMap<>();
+		headerMap.put(ACCT_NM,"Account Name");
+		headerMap.put(ACCT_EXPIRE,"Account Expiration");
+		headerMap.put(USER_EXPIRE,"User Expiration");
+		headerMap.put(COMPANY,"Company");
+		headerMap.put(TITLE,"Title");
+		headerMap.put(FIRST_NM,"First");
+		headerMap.put(LAST_NM,"Last");
+		headerMap.put(EMAIL,"Email Address");
+		headerMap.put(LAST_LOGIN,"Last Login");
+		//headerMap.put(HITS,"Hits");
+		headerMap.put(MAIN_PHONE,"Phone");
+		headerMap.put(MOBILE_PHONE,"Mobile Phone");
+		headerMap.put(ADDRESS1,"Address 1");
+		headerMap.put(ADDRESS2,"Address 2");
+		headerMap.put(CITY_NM,"City");
+		headerMap.put(STATE_CD,"State");
+		headerMap.put(POSTAL_CD,"Zip Code");
+		headerMap.put(COUNTRY_CD,"Country");
+		headerMap.put(USER_STATUS,"Status");
+		headerMap.put(SOURCE,"Source");
+		headerMap.put(UPDATES,"General Notifications");
+		headerMap.put(FAV_UPDATES,"Favorite Notifications");
+		headerMap.put(DATE_JOINED,"Date Joined");
+		headerMap.put(DIVISION,"Division");
+		//headerMap.put(OS,"OS");
+		//headerMap.put(BROWSER,"Browser");
+		headerMap.put(HAS_FD, USER_FD_VAL);
+		headerMap.put(NOTES,"Notes");
+		headerMap.put(COMPANY_URL,"Company URL");
+		headerMap.put(JOB_CATEGORY,"Job Category");
+		headerMap.put(JOB_LEVEL,"Job Level");
+		headerMap.put(INDUSTRY,"Industry");
+		headerMap.put(DATE_DEMOED,"Date Demoed");
+		headerMap.put(DATE_TRAINED,"Date Trained");
+		return headerMap;
+	}
+
+}
