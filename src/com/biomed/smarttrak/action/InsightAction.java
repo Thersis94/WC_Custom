e84dd3fcb1f27245c8c9b1c2c3066c7ca83678a5
Ryan <ryan@siliconmtn.com>
2017-03-05 18:10:07 -0700

    sonar cube

--- src/com/biomed/smarttrak/action/InsightAction.java
+++ src/com/biomed/smarttrak/action/InsightAction.java
@@ -199,8 +199,6 @@ public class InsightAction extends AbstractTreeAction {
 		List<Object>  insight = db.executeSelect(sb.toString(), params, new InsightVO());
 		log.debug("loaded " + insight.size() + " insight");
 		
-		//log.debug("placed vo on mod data: " + (InsightVO)insight.get(0) + "|"+(String)getAttribute(Constants.QS_PATH));
-		
 		for (Object vo : insight){
 			InsightVO ivo = (InsightVO)vo;
 			ivo.setQsPath((String)getAttribute(Constants.QS_PATH));
