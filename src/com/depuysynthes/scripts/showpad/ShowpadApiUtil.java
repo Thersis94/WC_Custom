6782abe26f7e4454f156ce22aaa0636dbf1a00a2
James McKain <jmckain@siliconmtn.com>
2017-03-16 21:15:21 -0400

    fixed counter incrementing:

--- src/com/depuysynthes/scripts/showpad/ShowpadApiUtil.java
+++ src/com/depuysynthes/scripts/showpad/ShowpadApiUtil.java
@@ -187,7 +187,7 @@ public class ShowpadApiUtil {
 			if (diff < 0) diff = 60 - Math.abs(diff); //think 9:10 - 8:55 
 			for (int x=diff; x > 0; x--) {
 				int idx = currentMinute-x;
-				if (idx < 0) idx = 60-idx; //reset to the top of the hour
+				if (idx < 0) idx = 60-Math.abs(idx); //reset to the top of the hour
 				minuteTotals[idx].set(0); //remember currentMinute was 5mins ago...add forward from there
 				log.debug("reset count on minute " + idx  + " to " + minuteTotals[idx].get());
 			}
