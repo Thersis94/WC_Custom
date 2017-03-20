06ead164cdf8f61fe6486639845958e76da7ab82
YetAnotherMask <edamschroder@siliconmtn.com>
2017-03-20 17:23:51 -0400

    Mirror changes to util.

--- src/com/biomed/smarttrak/admin/CRMAction.java
+++ src/com/biomed/smarttrak/admin/CRMAction.java
@@ -2,7 +2,7 @@ package com.biomed.smarttrak.admin;
 
 import com.biomed.crm.CRMUtil;
 import com.biomed.crm.CRMUtil.ActionType;
-import com.biomed.crm.CustomerVO;
+import com.biomed.crm.BiomedCRMCustomerVO;
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionRequest;
 import com.siliconmtn.security.UserDataVO;
@@ -32,10 +32,16 @@ public class CRMAction extends SBActionAdapter {
 		String msg = StringUtil.capitalizePhrase(buildAction) + " completed successfully.";
 		try {
 			CRMUtil util = new CRMUtil((String) attributes.get(Constants.DATA_FEED_SCHEMA), dbConn, attributes);
+			ActionType type;
+			if (req.hasParameter("actionTarget")) {
+				type = CRMUtil.ActionType.valueOf(req.getParameter("actionTarget"));
+			} else {
+				type = CRMUtil.ActionType.CUSTOMER;
+			}
 			if (req.hasParameter("alter")) {
-				util.alterElement(req);
+				util.alterElement(type, req);
 			} else if ("update".equals(buildAction)) {
-				util.updateElement(req);
+				util.updateElement(type, req);
 			}
 		} catch(Exception e) {
 			msg = StringUtil.capitalizePhrase(buildAction) + " failed to complete successfully. Please contact an administrator about this issue.";
@@ -49,22 +55,22 @@ public class CRMAction extends SBActionAdapter {
 	public void retrieve(ActionRequest req) throws ActionException {
 		CRMUtil util = new CRMUtil((String) attributes.get(Constants.DATA_FEED_SCHEMA), dbConn, attributes);
 		if ("customer".equals(req.getParameter("type"))) {
-			putModuleData(util.getCustomerInfo(null, true));
+			putModuleData(util.getCustomerInfo(null));
 		} else if ("deals".equals(req.getParameter("type"))) {
-			CustomerVO cust = new CustomerVO(req);
+			BiomedCRMCustomerVO cust = new BiomedCRMCustomerVO(req);
 			util.addDeals(cust);
 			putModuleData(cust);
 		} else if ("notes".equals(req.getParameter("type"))) {
-			CustomerVO cust = new CustomerVO(req);
+			BiomedCRMCustomerVO cust = new BiomedCRMCustomerVO(req);
 			util.addNotes(cust);
 			putModuleData(cust);
 		} else if ("reminders".equals(req.getParameter("type"))) {
 			UserDataVO user = (UserDataVO) req.getSession().getAttribute(Constants.USER_DATA);
-			CustomerVO cust = new CustomerVO(req);
+			BiomedCRMCustomerVO cust = new BiomedCRMCustomerVO(req);
 			util.addReminders(cust, StringUtil.checkVal(user.getProfileId()));
 			putModuleData(cust);
 		} else if (!req.hasParameter("add") && req.hasParameter("customerId")) {
-			putModuleData(util.getCustomerInfo(req.getParameter("customerId"), true).get(0));
+			putModuleData(util.getCustomerInfo(req.getParameter("customerId")).get(0));
 		} 
 	}
 
