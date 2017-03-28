64ecdd1037b22f37b5658a27f9ddb38b3ce0aab1
YetAnotherMask <edamschroder@siliconmtn.com>
2017-03-28 16:52:31 -0400

    Increase information sent back for lists.

--- src/com/biomed/smarttrak/admin/CompanyManagementAction.java
+++ src/com/biomed/smarttrak/admin/CompanyManagementAction.java
@@ -317,7 +317,8 @@ public class CompanyManagementAction extends AbstractTreeAction {
 		List<Object> params = new ArrayList<>();
 		String customDb = (String)attributes.get(Constants.CUSTOM_DB_SCHEMA);
 		StringBuilder sql = new StringBuilder(400);
-		sql.append("select c.COMPANY_NM, c.COMPANY_ID, CASE WHEN (ci.investee_company_id  is null) THEN 0 ELSE 1 end as INVESTED_FLG ");
+		sql.append("select c.COMPANY_NM, c.COMPANY_ID, c.COMPLETION_SCORE_NO, c.STATUS_NO, ");
+		sql.append("CASE WHEN (ci.investee_company_id  is null) THEN 0 ELSE 1 end as INVESTED_FLG ");
 		sql.append("FROM ").append(customDb).append("BIOMEDGPS_COMPANY c ");
 		sql.append("left join ").append(customDb).append("biomedgps_company_investor ci ");
 		sql.append("on c.COMPANY_ID = ci.investee_company_id ");
