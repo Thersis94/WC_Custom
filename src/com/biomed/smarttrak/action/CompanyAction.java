c3118825e9f9f48e4ac2a60622c97bd952b4ea4e
YetAnotherMask <edamschroder@siliconmtn.com>
2017-04-18 16:01:23 -0400

    Add page title setting for detail pages.

--- src/com/biomed/smarttrak/action/CompanyAction.java
+++ src/com/biomed/smarttrak/action/CompanyAction.java
@@ -28,6 +28,7 @@ import com.siliconmtn.util.StringUtil;
 import com.smt.sitebuilder.action.search.SolrAction;
 import com.smt.sitebuilder.common.ModuleVO;
 import com.smt.sitebuilder.common.PageVO;
+import com.smt.sitebuilder.common.SiteVO;
 import com.smt.sitebuilder.common.constants.Constants;
 import com.smt.sitebuilder.search.SearchDocumentHandler;
 import com.smt.sitebuilder.util.solr.SecureSolrDocumentVO.Permission;
@@ -71,6 +72,9 @@ public class CompanyAction extends AbstractTreeAction {
 				sbUtil.manualRedirect(req,page.getFullPath());
 			} else {
 				SecurityController.getInstance(req).isUserAuthorized(vo, req);
+			    	PageVO page = (PageVO)req.getAttribute(Constants.PAGE_DATA);
+			    	SiteVO site = (SiteVO)req.getAttribute(Constants.SITE_DATA);
+				page.setTitleName(vo.getCompanyName() + " | " + site.getSiteName());
 				putModuleData(vo);
 			}
 		} else if (req.hasParameter("searchData") || req.hasParameter("fq") || req.hasParameter("hierarchyList")){
