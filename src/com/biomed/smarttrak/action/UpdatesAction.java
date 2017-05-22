1f4cd17dd1db4d4d3e6de8380bb89bfc239b12a0
devonSMT <devon.franklin93@gmail.com>
2017-05-22 14:28:02 -0600

    Updating per sonarqube.

--- src/com/biomed/smarttrak/action/UpdatesAction.java
+++ src/com/biomed/smarttrak/action/UpdatesAction.java
@@ -255,19 +255,26 @@ public class UpdatesAction extends SBActionAdapter {
 			data.add(SearchDocumentHandler.MODULE_TYPE + ":" + req.getParameter("typeId"));
 
 		//Custom Filtering for when looking at an Email View.
-		if(req.hasParameter("isEmail")) {
-
-			//Set fmid from ModuleVO
-			ModuleVO mod = (ModuleVO) attributes.get(Constants.MODULE_DATA);
-			req.setParameter("fmid", mod.getPageModuleId());
-
-			//Set Custom Sort Field and Direction.
-			req.setParameter("sortField", "moduleType asc, publishDtNoTime_s desc, order_i asc, publishTime_s ");
-			req.setParameter("sortDirection", "desc");
-		}
+		transposeEmailFilter(req);
 
 		//put the new list of filter queries back on the request
 		req.setParameter("fq", data.toArray(new String[data.size()]), true);
 		req.setParameter("ft", terms.toArray(new String[terms.size()]), true);
+	}
+	
+	/**
+	 * Handles processing the custom filter for email views for Solr
+	 * @param req
+	 */
+	protected void transposeEmailFilter(ActionRequest req){
+		if(!req.hasParameter("isEmail")) return;
+		
+		//Set fmid from ModuleVO
+		ModuleVO mod = (ModuleVO) attributes.get(Constants.MODULE_DATA);
+		req.setParameter("fmid", mod.getPageModuleId());
+
+		//Set Custom Sort Field and Direction.
+		req.setParameter("sortField", "moduleType asc, publishDtNoTime_s desc, order_i asc, publishTime_s ");
+		req.setParameter("sortDirection", "desc");
 	}
 }
