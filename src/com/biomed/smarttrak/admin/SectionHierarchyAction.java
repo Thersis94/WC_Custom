ae666b0367a39c4d6cace479d21c7e03d45eb807
YetAnotherMask <edamschroder@siliconmtn.com>
2017-06-05 09:38:41 -0400

    Change return to empty list instead of null.

--- src/com/biomed/smarttrak/admin/SectionHierarchyAction.java
+++ src/com/biomed/smarttrak/admin/SectionHierarchyAction.java
@@ -137,7 +137,7 @@ public class SectionHierarchyAction extends AbstractTreeAction {
 	 * @param role
 	 */
 	protected List<Node> checkPermissions(List<Node> sections, SmarttrakRoleVO role) {
-		if (sections == null || sections.isEmpty()) return null;
+		if (sections == null || sections.isEmpty()) return new ArrayList<Node>();
 		
 		String[] roleAcl = role.getAuthorizedSections();
 		List<Node> allowed = new ArrayList<>();
