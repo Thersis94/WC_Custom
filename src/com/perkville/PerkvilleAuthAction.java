1ca02ecea1f36fc120be360dab87195ecd9266b5
billysmt <billy@siliconmtn.com>
2017-11-29 19:48:51 -0500

    Cleaned up Perkville Api for OAuth

--- src/com/perkville/PerkvilleAuthAction.java
+++ src/com/perkville/PerkvilleAuthAction.java
@@ -1,18 +1,21 @@
 package com.perkville;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.auth.oauth2.Credential;
-import com.perkville.PerkvilleOAuth2Token.Config;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
+import com.siliconmtn.security.BaseOAuth2Token.Config;
 import com.siliconmtn.security.UserDataVO;
 import com.siliconmtn.util.StringUtil;
 import com.smt.sitebuilder.action.SimpleActionAdapter;
 import com.smt.sitebuilder.common.SiteVO;
 import com.smt.sitebuilder.common.constants.Constants;
+import com.smt.sitebuilder.security.oauth.OAuth2TokenViaDB;
 
 /****************************************************************************
  * <b>Title:</b> PerkvilleAuthAction.java
@@ -27,6 +30,32 @@ import com.smt.sitebuilder.common.constants.Constants;
  ****************************************************************************/
 public class PerkvilleAuthAction extends SimpleActionAdapter {
 
+	public enum Scope {
+		PUBLIC,						//Grants read-only access to public information
+		USER_CUSTOMER_INFO,			//Grants read-only access to user's contact information, business connections, and vouchers. Grants ability to mark a voucher as "Used".
+		USER_REDEEM,					//Grants ability to redeem a user's earned points
+		USER_REFERRAL,				//Grants ability to create a referral to a users connected businesses
+		ADMIN_CUSTOMER_INFO,			//Grants read-only access to a user's customer's contact information and business connections.
+		ADMIN_CUSTOMER_REDEEM,		//Grants read access to a user's customer's vouchers. Grants ability to mark customer's vouchers as "Used". Grants ability to redeem points on behalf of customers.
+		ADMIN_CUSTOMER_GRANT_POINTS,	//Grants ability to grant points to a user's customers and invite new customers to Perkville.
+		ADMIN_CUSTOMER_REFERRAL,		//Grants ability to create a referral on behalf of two users.
+		ADMIN_PERK,					//Grants ability to create and edit perks within a business' rewards program.
+		ADMIN_LOCATION				//Grants ability to create and edit a business's locations.
+	}
+
+	public enum PVSiteConfig {
+		PV_API_KEY,
+		PV_API_SECRET,
+		PV_TOKEN_CALLBACK_URL,
+		PV_TOKEN_SERVER_URL,
+		PV_AUTH_SERVER_URL,
+		PV_KEYSTORE,
+		PV_STATE,
+		PV_SCOPE,
+		PV_AUTH_CODE_RESPONSE,
+		PV_GRANT_TYPE_CODE
+	}
+
 	public PerkvilleAuthAction() {
 		super();
 	}
@@ -37,77 +66,94 @@ public class PerkvilleAuthAction extends SimpleActionAdapter {
 
 	@Override
 	public void retrieve(ActionRequest req) {
+		processOAuthCall(req);
+	}
 
-		//Build map of config information.
-		Map<Config, String> config = buildConfigMap(req);
+	private void processOAuthCall(ActionRequest req) {
+		Map<String, String> siteConfig = ((SiteVO)req.getAttribute(Constants.SITE_DATA)).getSiteConfig();
+		UserDataVO user = (UserDataVO) req.getSession().getAttribute(Constants.USER_DATA);
 		Credential c = null;
 
 		try {
 
 			//Initialize Token.
-			PerkvilleOAuth2Token token = new PerkvilleOAuth2Token(config);
+			PerkvilleOAuth2Token token = new PerkvilleOAuth2Token(buildConfigMap(siteConfig, user), buildScopes(siteConfig), buildOAuthAttributes(req, siteConfig));
 
 			//Attempt to get Token
 			c = token.getToken();
 
 			//If Null and on Step 1, redirect User to to Perkville Auth url 
 			if (c == null && !req.hasParameter("code")) {
-				log.debug("creating access token");
-				//we need to pause here and prompt the user to go get us an access token.  An interactive web browser is required to do this.
-				AuthorizationCodeRequestUrl u = token.getFlow().newAuthorizationUrl();
-				u.setRedirectUri(config.get(Config.PV_TOKEN_CALLBACK_URL));
-				sendRedirect(u.toURL().toString(), "", req);
+				sendRedirect(token.buildAuthReqUrl(), "", req);
 			}
 
 			//If Null and on Step 2, get the code param off the req and request Token.
 			else if(c == null && req.hasParameter("code")) {
 				// receive authorization code and exchange it for an access token
-				c = token.processResponse(req.getParameter("code"));
+				c = token.getToken(req.getParameter("code"));
 			}
 
 			//Refresh Token.
 			else {
-				log.debug("refreshing access token");
-				c = token.getFlow().createAndStoreCredential(token.requestRefreshToken(c), config.get(Config.PV_CLIENT_ID));
+				log.debug("refreshing access token" + req.getParameter("access_token"));
+				//c = token.getFlow().createAndStoreCredential(token.requestRefreshToken(c), config.get(Config.PV_CLIENT_ID));
 			}
 
 		} catch(Exception e) {
 			log.error("Problem navigating Perkville OAuth.", e);
 		}
-
-		//If we have a Credential Object, Persist the Credentials Access Token.
-		if(c != null) {
-			persistCredentials(c.getAccessToken());
-		}
 	}
 
 	/**
-	 * Store Access Token long term.  Probably in Database.
-	 * @param c
+	 * Build the Attributes map for the PerkvilleOAuth2Token.
+	 * @param req 
+	 * @param siteConfig
+	 * @return
 	 */
-	private void persistCredentials(String token) {
-		log.debug("HAVE TOKEN: " + token);
-		//TODO - Discuss how/where to store Access Token.
+	private Map<String, Object> buildOAuthAttributes(ActionRequest req, Map<String, String> siteConfig) {
+		Map<String, Object> attr = new HashMap<>();
+		attr.put(OAuth2TokenViaDB.DB_CONN_KEY, dbConn);
+		attr.put(PerkvilleOAuth2Token.RESPONSE_TYPE_PARAM, siteConfig.get(PVSiteConfig.PV_AUTH_CODE_RESPONSE.name()));
+		attr.put(PerkvilleOAuth2Token.CODE_PARAM, req.getParameter(PerkvilleOAuth2Token.CODE_PARAM));
+		return attr;
 	}
 
 	/**
+	 * Build List of Scopes for PerkvilleOAuth2Token.
+	 * @param siteConfig
+	 * @return
+	 */
+	private List<String> buildScopes(Map<String, String> siteConfig) {
+		List<String> scopes = new ArrayList<>();
+		if(siteConfig.containsKey(PVSiteConfig.PV_SCOPE.name())) {
+			String [] strArr = StringUtil.checkVal(siteConfig.get(PVSiteConfig.PV_SCOPE.name())).split("\\+");
+			for(String s : strArr) {
+				scopes.add(Scope.valueOf(s).name());
+			}
+		} else {
+			scopes.add(Scope.PUBLIC.name());
+		//	scopes.add(Scope.USER_CUSTOMER_INFO.name());
+		}
+
+		return scopes;
+	}
+
+	/**
+	 * Build the Config map for the PerkvilleOAuth2Token
 	 * @param req
 	 * @return
 	 */
-	private Map<Config, String> buildConfigMap(ActionRequest req) {
-		Map<String, String> siteConfig = ((SiteVO)req.getAttribute(Constants.SITE_DATA)).getSiteConfig();
+	private Map<Config, String> buildConfigMap(Map<String, String> siteConfig, UserDataVO user) {
 
 		Map<Config, String> configMap = new EnumMap<>(Config.class);
-		configMap.put(Config.PV_CLIENT_ID, siteConfig.get(Config.PV_CLIENT_ID.name()));
-		configMap.put(Config.PV_API_SECRET, siteConfig.get(Config.PV_API_SECRET.name())); 
-		configMap.put(Config.PV_TOKEN_CALLBACK_URL, siteConfig.get(Config.PV_TOKEN_CALLBACK_URL.name()));
-		configMap.put(Config.PV_TOKEN_SERVER_URL, siteConfig.get(Config.PV_TOKEN_SERVER_URL.name()));
-		configMap.put(Config.PV_AUTH_SERVER_URL, siteConfig.get(Config.PV_AUTH_SERVER_URL.name()));
-		configMap.put(Config.PV_KEYSTORE, "perkville-" + StringUtil.removeNonAlphaNumeric(siteConfig.get(Config.PV_CLIENT_ID.name())));
-		configMap.put(Config.PV_STATE, siteConfig.get(Config.PV_STATE.name()));
-		configMap.put(Config.PV_SCOPE, siteConfig.get(Config.PV_SCOPE.name()));
-		configMap.put(Config.PV_AUTH_CODE_RESPONSE, siteConfig.get(Config.PV_AUTH_CODE_RESPONSE.name()));
-		configMap.put(Config.PV_GRANT_TYPE_CODE, siteConfig.get(Config.PV_GRANT_TYPE_CODE.name()));
+		configMap.put(Config.USER_ID, user.getProfileId());
+		configMap.put(Config.API_KEY, siteConfig.get(PVSiteConfig.PV_API_KEY.name()));
+		configMap.put(Config.API_SECRET, siteConfig.get(PVSiteConfig.PV_API_SECRET.name())); 
+		configMap.put(Config.TOKEN_CALLBACK_URL, siteConfig.get(PVSiteConfig.PV_TOKEN_CALLBACK_URL.name()));
+		configMap.put(Config.TOKEN_SERVER_URL, siteConfig.get(PVSiteConfig.PV_TOKEN_SERVER_URL.name()));
+		configMap.put(Config.AUTH_SERVER_URL, siteConfig.get(PVSiteConfig.PV_AUTH_SERVER_URL.name()));
+		configMap.put(Config.KEYSTORE, PerkvilleOAuth2Token.PERKVILLE_KEYSTORE);
+		configMap.put(Config.GRANT_TYPE, siteConfig.get(PVSiteConfig.PV_GRANT_TYPE_CODE.name()));
 		return configMap;
 	}
 }
--- src/com/perkville/PerkvilleOAuth2Token.java
+++ src/com/perkville/PerkvilleOAuth2Token.java
@@ -1,24 +1,15 @@
 package com.perkville;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.RefreshTokenRequest;
+import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.http.BasicAuthentication;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.siliconmtn.security.OAuth2Token;
-import com.siliconmtn.util.StringUtil;
+import com.siliconmtn.http.parser.StringEncoder;
+import com.smt.sitebuilder.security.oauth.OAuth2TokenViaDB;
 
 /****************************************************************************
  * <b>Title:</b> PerkvilleOAuth2Token.java
@@ -31,165 +22,87 @@ import com.siliconmtn.util.StringUtil;
  * @version 3.3.1
  * @since Nov 28, 2017
  ****************************************************************************/
-public class PerkvilleOAuth2Token implements OAuth2Token {
-
-	protected static Logger log;
-	
-	/**
-	 * how close we're willing to get to an expired token before refreshing it
-	 */
-	protected static final int TIMEOUT_BUFFER_SECS = 30;
+public class PerkvilleOAuth2Token extends OAuth2TokenViaDB {
 
 	public static final String CLIENT_ID_PARAM = "client_id";
-	public static final String REDIRECT_URL_PARAM = "redirect_url";
+	public static final String REDIRECT_URL_PARAM = "redirect_uri";
 	public static final String RESPONSE_TYPE_PARAM = "response_type";
 	public static final String SCOPE_PARAM = "scope";
 	public static final String STATE_PARAM = "state";
 	public static final String GRANT_TYPE_PARAM = "grant_type";
 	public static final String CODE_PARAM = "code";
 
-	private Map<Config, String> config = null;
+	public static final String PERKVILLE_KEYSTORE = "perkville";
 
-	/** 
-	 * Change Scopes to match those you've authorized in your Showpad account for this user.
-	 */
-	private Collection<String> scopes = null;
+	public PerkvilleOAuth2Token(Map<Config, String> config, List<String> scopes, Map<String, Object> attributes) throws IOException {
+		super(config, scopes, attributes);
+	}
 
-	private AuthorizationCodeFlow flow = null;
 	/**
-	 * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
-	 * globally shared instance across your application.
-	 */
-	private FileDataStoreFactory dataStoreFactory;
-
-	private Credential token;
-	public enum SCOPE {
-		PUBLIC,						//Grants read-only access to public information
-		USER_CUSTOMER_INFO,			//Grants read-only access to user's contact information, business connections, and vouchers. Grants ability to mark a voucher as "Used".
-		USER_REDEEM,					//Grants ability to redeem a user's earned points
-		USER_REFERRAL,				//Grants ability to create a referral to a users connected businesses
-		ADMIN_CUSTOMER_INFO,			//Grants read-only access to a user's customer's contact information and business connections.
-		ADMIN_CUSTOMER_REDEEM,		//Grants read access to a user's customer's vouchers. Grants ability to mark customer's vouchers as "Used". Grants ability to redeem points on behalf of customers.
-		ADMIN_CUSTOMER_GRANT_POINTS,	//Grants ability to grant points to a user's customers and invite new customers to Perkville.
-		ADMIN_CUSTOMER_REFERRAL,		//Grants ability to create a referral on behalf of two users.
-		ADMIN_PERK,					//Grants ability to create and edit perks within a business' rewards program.
-		ADMIN_LOCATION				//Grants ability to create and edit a business's locations.
-	}
-
-	public enum Config {
-		PV_CLIENT_ID,
-		PV_API_SECRET, 
-		PV_TOKEN_CALLBACK_URL,
-		PV_TOKEN_SERVER_URL,
-		PV_AUTH_SERVER_URL,
-		PV_KEYSTORE,
-		PV_STATE,
-		PV_SCOPE,
-		PV_AUTH_CODE_RESPONSE,
-		PV_GRANT_TYPE_CODE
-	}
-
-	public PerkvilleOAuth2Token(Map<Config, String> config) throws IOException {
-		if (config == null || config.size() != Config.values().length) {
-			throw new IOException("Config missing or incomplete.  You must implement all values in the Param enum.");
-		}
-		log = Logger.getLogger(this.getClass());
-		this.config = config;
-		buildScopes(config.get(Config.PV_SCOPE));
-
-		//TODO - How to store Credentials?
-		File dataStoreDir = new File(System.getProperty("user.home"), ".store/" + config.get(Config.PV_KEYSTORE));
-		dataStoreFactory = new FileDataStoreFactory(dataStoreDir);
-
-		//load the token so we're ready to go.
-		//We want to tell the invoking class early if this isn't going to work; they likely depend on it.
-		token = obtainToken();
-	}
-
-	/* (non-Javadoc)
-	 * @see com.siliconmtn.security.OAuth2Token#getToken()
+	 * Builds appropriate Auth URL for Perkville.
+	 * @return the Auth Request Url.
 	 */
 	@Override
-	public Credential getToken() {
-		//renew the token if it expires in the next 30 seconds; it may be worthless by the time we're ready to use it. -JM 02.16.16
-		if (token == null || token.getExpiresInSeconds() == null || token.getExpiresInSeconds() <= TIMEOUT_BUFFER_SECS) {
-			if (token != null) log.debug("token timeout in " + token.getExpiresInSeconds() + " seconds");
-			try {
-				token = obtainToken();
-			} catch (Exception e) {
-				log.error("could not obtain access token", e);
-			}
-		}
-		return token;
-	}
+	public String buildAuthReqUrl() {
+		GenericUrl url = new GenericUrl(config.get(Config.AUTH_SERVER_URL));
+	    url.set(CLIENT_ID_PARAM, config.get(Config.API_KEY));
+	    url.set(RESPONSE_TYPE_PARAM, attributes.get(RESPONSE_TYPE_PARAM));
+	    url.set(SCOPE_PARAM, buildScopeString());
+	    url.set(STATE_PARAM, attributes.get(STATE_PARAM));
 
-	private Credential obtainToken() throws IOException {
-		AuthorizationCodeFlow.Builder builder = new AuthorizationCodeFlow.Builder(
-				BearerToken.authorizationHeaderAccessMethod(),
-				transport,
-				jsonFactory,
-				new GenericUrl(buildAuthRequestUrl()),
-				new BasicAuthentication(config.get(Config.PV_CLIENT_ID), config.get(Config.PV_API_SECRET)),
-				config.get(Config.PV_CLIENT_ID),
-				config.get(Config.PV_AUTH_SERVER_URL));
-		builder.setScopes(scopes);
-		builder.setDataStoreFactory(dataStoreFactory);
+		//GenericUrl is encoding the CallbackUrl in a way that Perkville doesn't recognize so append it after the fact.
+	    StringBuilder u = new StringBuilder(150);
+	    u.append(url.build()).append("&").append(REDIRECT_URL_PARAM).append("=").append(StringEncoder.urlEncode(config.get(Config.TOKEN_CALLBACK_URL)));
+		log.info(u);
 
-		flow = builder.build();
-		return flow.loadCredential(config.get(Config.PV_CLIENT_ID));
+	    return u.toString();
 	}
 
 	/**
-	 * creates and sends a RefreshTokenRequest to the OAUTH2 remote server.
-	 * The returned TokenResponse must be captured by the Flow and persisted to 
-	 * the keystore for next time.
-	 * @param c
+	 * Build the Token Request Url.
+	 */
+	@Override
+	public String buildTokenReqUrl() {
+		GenericUrl url = new GenericUrl(config.get(Config.TOKEN_SERVER_URL));
+		url.set(CLIENT_ID_PARAM, config.get(Config.API_KEY));
+		url.set(CODE_PARAM, attributes.get(CODE_PARAM));
+		url.set(GRANT_TYPE_PARAM, config.get(Config.GRANT_TYPE));
+		url.set(STATE_PARAM, attributes.get(STATE_PARAM));
+
+		//GenericUrl is encoding the CallbackUrl in a way that Perkville doesn't recognize so append it after the fact.
+		StringBuilder u = new StringBuilder(150);
+	    u.append(url.build()).append("&").append(REDIRECT_URL_PARAM).append("=").append(StringEncoder.urlEncode(config.get(Config.TOKEN_CALLBACK_URL)));
+		log.info(u);
+
+	    return u.toString();
+	}
+
+	/**
+	 * Build the Scope String according to how Perkville wants it.
+	 * @return
+	 */
+	private String buildScopeString() {
+		StringBuilder scopeStr = new StringBuilder(scopes.size() * 15);
+		for(String s: scopes) {
+			scopeStr.append(s).append(" ");
+		}
+
+		return scopeStr.toString().trim();
+	}
+
+	/**
+	 * Build the TokenResponse Object with the provided code.
+	 * @param code - Response from the OAuth Endpoint.
 	 * @return
 	 * @throws IOException
 	 */
-	public TokenResponse requestRefreshToken(Credential c) throws IOException {
-		RefreshTokenRequest authReq = new RefreshTokenRequest(
-				transport, 
-				jsonFactory,
-				new GenericUrl(config.get(Config.PV_TOKEN_SERVER_URL)), 
-				c.getRefreshToken());
-		authReq.setClientAuthentication(new BasicAuthentication(config.get(Config.PV_CLIENT_ID), config.get(Config.PV_API_SECRET)));
-		authReq.setGrantType(config.get(Config.PV_GRANT_TYPE_CODE));
+	public TokenResponse getTokenResponse(String code) throws IOException {
+		AuthorizationCodeTokenRequest tokReq = flow.newTokenRequest(code)
+		.setRedirectUri(config.get(Config.TOKEN_CALLBACK_URL))
+		.setScopes(scopes);
+		tokReq.setClientAuthentication(new BasicAuthentication(config.get(Config.API_KEY), config.get(Config.API_SECRET)));
 
-		return authReq.execute();
-	}
-
-	/**
-	 * client_id		Yes		The client id that was emailed to the developer contact upon client registration
-	 * 
-	 * redirect_uri	No		The redirect uri that you would like Perkville to redirect your user to once they
-	 * 						have authorized your application to access their data on Perkville. Important:
-	 * 						This MUST match one of the redirect uris that was provided upon client registration.
-	 * 						If this querystring parameter is omitted, Perkville will default to the first redirect
-	 * 						uri it has stored for the provided client id.
-	 * 
-	 * response_type	Yes		This must be "code" for the authorization code flow.
-	 * 
-	 * scope			No		The scopes that you would like to request access to on behalf of the user, separated by
-	 * 						spaces. See below for a detailed explanation of the allowed scopes. If not provided,
-	 * 						defaults to all of the scopes that your client application is allowed to request.
-	 * 
-	 * state			No		It is recommended that the client application populate this query parameter with a random
-	 * 						hash. When Perkville redirects the user back to the client server, this state will be
-	 * 						included in the redirect request query string so that the client servers may verify
-	 * 						the authenticity of the request.
-	 * 
-	 * @return the Auth Request Url.
-	 */
-	public String buildAuthRequestUrl() {
-		GenericUrl url = new GenericUrl(config.get(Config.PV_AUTH_SERVER_URL));
-	    url.set(CLIENT_ID_PARAM, config.get(Config.PV_CLIENT_ID));
-	    url.set(REDIRECT_URL_PARAM, config.get(Config.PV_TOKEN_CALLBACK_URL));
-	    url.set(RESPONSE_TYPE_PARAM, config.get(Config.PV_AUTH_CODE_RESPONSE));
-	    url.set(SCOPE_PARAM, config.get(Config.PV_SCOPE));
-	    url.set(STATE_PARAM, config.get(Config.PV_STATE));
-
-	    return url.build();
+		return tokReq.execute();
 	}
 
 	/**
@@ -198,54 +111,13 @@ public class PerkvilleOAuth2Token implements OAuth2Token {
 	 * @return
 	 */
 	public String buildTokenRequestUrl(String code) {
-		GenericUrl url = new GenericUrl(config.get(Config.PV_TOKEN_SERVER_URL));
-		url.set(CLIENT_ID_PARAM, config.get(Config.PV_CLIENT_ID));
+		GenericUrl url = new GenericUrl(config.get(Config.TOKEN_SERVER_URL));
+		url.set(CLIENT_ID_PARAM, config.get(Config.API_KEY));
 		url.set(CODE_PARAM, code);
-		url.set(GRANT_TYPE_PARAM, config.get(Config.PV_GRANT_TYPE_CODE));
-		url.set(REDIRECT_URL_PARAM, config.get(Config.PV_TOKEN_CALLBACK_URL));
-		url.set(STATE_PARAM, config.get(Config.PV_STATE));
+		url.set(GRANT_TYPE_PARAM, attributes.get(GRANT_TYPE_PARAM));
+		url.set(REDIRECT_URL_PARAM, Config.TOKEN_CALLBACK_URL);
+		url.set(STATE_PARAM, attributes.get(STATE_PARAM));
 
 		return url.build();
-	}
-
-	public AuthorizationCodeFlow getFlow() {
-		return flow;
-	}
-
-	/**
-	 * Build List of Perkville Scopes we want for the user.
-	 * @return
-	 */
-	public void buildScopes(String scopeConfig) {
-		scopes = new ArrayList<>();
-		if(!StringUtil.isEmpty(scopeConfig) && scopeConfig.indexOf('+') > -1) {
-			String [] sArr = scopeConfig.split("\\+");
-			for(String s : sArr) {
-				try {
-					scopes.add(SCOPE.valueOf(s).name());
-				} catch(Exception e) {
-					log.error("Scope " + s + " is not a valid Perkville Scope.");
-				}
-			}
-		} else {
-			scopes.add(SCOPE.PUBLIC.name());
-			scopes.add(SCOPE.USER_CUSTOMER_INFO.name());
-		}
-	}
-
-	/**
-	 * @param parameter
-	 * @return
-	 * @throws IOException 
-	 */
-	public Credential processResponse(String code) throws IOException {
-		TokenResponse response;
-		response = getFlow().newTokenRequest(code)
-				.setRedirectUri(config.get(Config.PV_TOKEN_CALLBACK_URL))
-				.setScopes(scopes)
-				.execute();
-
-		// store credential and return it
-		return getFlow().createAndStoreCredential(response, config.get(Config.PV_CLIENT_ID));
 	}
 }
