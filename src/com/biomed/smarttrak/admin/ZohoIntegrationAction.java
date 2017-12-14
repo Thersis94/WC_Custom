d90ea8ffa5c6cb3221dab7c419034ce796229a8b
YetAnotherMask <edamschroder@siliconmtn.com>
2017-12-14 11:35:10 -0500

    Add support for chaching the results from the zoho retrieve to reduce load times and reduce chances of reaching the one hundred requests per two minutes limit the zoho rest api enforces.

--- src/com/biomed/smarttrak/admin/ZohoIntegrationAction.java
+++ src/com/biomed/smarttrak/admin/ZohoIntegrationAction.java
@@ -1,5 +1,6 @@
 package com.biomed.smarttrak.admin;
 
+
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
@@ -7,6 +8,9 @@ import com.siliconmtn.util.Convert;
 import com.smt.sitebuilder.action.ticketing.SupportUtilFactory;
 import com.smt.sitebuilder.action.ticketing.TicketFacadeAction;
 import com.smt.sitebuilder.action.ticketing.vo.SupportFieldsVO;
+import com.smt.sitebuilder.common.ModuleVO;
+import com.smt.sitebuilder.common.constants.Constants;
+import com.smt.sitebuilder.util.CacheAdministrator;
 
 /****************************************************************************
  * <b>Title</b>: ZohoIntegrationAction.java
@@ -25,6 +29,9 @@ public class ZohoIntegrationAction extends TicketFacadeAction {
 
 	// ID for the Smarttrak Development project
 	public static final String PROJECT_ID = "967168000000259067";
+	private static final String SMARTTRAK_CACHE_KEY = "smarttrak_zoho_ticket_list";
+	private static final int CACHE_TIMEOUT = 900;
+	
 	public ZohoIntegrationAction() {
 		super();
 	}
@@ -42,11 +49,31 @@ public class ZohoIntegrationAction extends TicketFacadeAction {
 		req.setParameter("projectId", PROJECT_ID);
 		req.setParameter(TICKET_TYPE_PARAM, TICKET_TYPE.BUG.toString());
 		if (Convert.formatBoolean(req.getParameter("loadData"))) {
-			super.list(req);
+			retrieveTickets(req);
 		} else {
 			// Only load the support fields if they are not present in
 			// order to reduce calls out to zoho for information.
 			loadSupportFields(req);
+		}
+	}
+	
+	/**
+	 * Determine whether the ticket information can be laoded from cache or if it needs to
+	 * be loaded from zoho. If it is loaded from zoho ensure that it is cached as well.
+	 * @param req
+	 * @throws ActionException
+	 */
+	private void retrieveTickets(ActionRequest req) throws ActionException {
+		CacheAdministrator cache = new CacheAdministrator(attributes);
+		ModuleVO cachedMod = cache.readFromCache(SMARTTRAK_CACHE_KEY);
+		if (cachedMod == null) {
+			super.list(req);
+			cachedMod = (ModuleVO) attributes.get(Constants.MODULE_DATA);
+			cachedMod.setCacheTimeout(CACHE_TIMEOUT);
+			cachedMod.setPageModuleId(SMARTTRAK_CACHE_KEY);
+			cache.writeToCache(cachedMod);
+		} else {
+			super.putModuleData(cachedMod.getActionData(), cachedMod.getDataSize(), false);
 		}
 	}
 	
@@ -64,6 +91,9 @@ public class ZohoIntegrationAction extends TicketFacadeAction {
 		req.setParameter("projectId", PROJECT_ID);
 		req.setParameter(TICKET_TYPE_PARAM, TICKET_TYPE.BUG.toString());
 		super.build(req);
+
+		CacheAdministrator cache = new CacheAdministrator(attributes);
+		cache.clearCacheByKey(SMARTTRAK_CACHE_KEY);
 	}
 
 }
