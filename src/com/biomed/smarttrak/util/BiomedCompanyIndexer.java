4afe0d25c2d201e4b102642c2fa840d8a8557dca
YetAnotherMask <edamschroder@siliconmtn.com>
2018-01-02 16:25:58 -0500

    Exclude Investor companies from being placed into solr.

--- src/com/biomed/smarttrak/util/BiomedCompanyIndexer.java
+++ src/com/biomed/smarttrak/util/BiomedCompanyIndexer.java
@@ -379,7 +379,7 @@ public class BiomedCompanyIndexer  extends SMTAbstractIndex {
 		sql.append("ON cs.COMPANY_ID = c.COMPANY_ID ");
 		sql.append("LEFT JOIN ").append(customDb).append("BIOMEDGPS_COMPANY c2 ");
 		sql.append("ON c2.COMPANY_ID = c.PARENT_ID ");
-		sql.append("WHERE c.STATUS_NO not in ('A','D') and p.STATUS_NO not in ('A', 'D', 'E') ");
+		sql.append("WHERE c.STATUS_NO not in ('A','D','I') and p.STATUS_NO not in ('A', 'D', 'E') ");
 		if (id != null) sql.append("and c.COMPANY_ID = ? ");
 		sql.append("GROUP BY c.COMPANY_ID, c.COMPANY_NM, a.SECTION_ID, c.STATUS_NO, ");
 		sql.append("c.stock_abbr_txt, p.COMPANY_ID, c2.COMPANY_NM, c.CREATE_DT, c.UPDATE_DT ");
