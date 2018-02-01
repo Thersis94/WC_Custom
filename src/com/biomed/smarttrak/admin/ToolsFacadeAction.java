d6dc6fc1e02f974f1bf9412ddea90039fd16c6f7
devonSMT <devon.franklin93@gmail.com>
2018-02-01 13:04:59 -0700

    feat: Add build method for product explorer preview. Refactoring of class.

--- src/com/biomed/smarttrak/admin/ToolsFacadeAction.java
+++ src/com/biomed/smarttrak/admin/ToolsFacadeAction.java
@@ -1,8 +1,5 @@
 package com.biomed.smarttrak.admin;
 
-//WC_Custom libs
-import com.biomed.smarttrak.action.GapAnalysisAction;
-import com.biomed.smarttrak.action.ProductExplorer;
 //Base libs
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
@@ -35,15 +32,21 @@ public class ToolsFacadeAction extends SBActionAdapter {
 	} 
 	
 	protected enum FacadeType{
-		PRODUCT_EXPLORER("explorer"), GAP_ANALYSIS("analysis");
+		PRODUCT_EXPLORER("explorer", "com.biomed.smarttrak.action.ProductExplorer"), 
+		GAP_ANALYSIS("analysis", "com.biomed.smarttrak.action.GapAnalysisAction");
 		
-		private String facadeTarget;
-		private FacadeType(String facadeTarget) {
+		private String facadeTarget; 
+		private String className; //the fully qualified class name
+		private FacadeType(String facadeTarget, String className) {
 			this.facadeTarget = facadeTarget;
+			this.className = className;
 		}
 		/*===Getters===*/
 		public String getFacadeTarget() {
 			return facadeTarget;
+		}
+		public String getClassName() {
+			return className;
 		}
 	}
 	
@@ -55,46 +58,61 @@ public class ToolsFacadeAction extends SBActionAdapter {
 	public void retrieve (ActionRequest req) throws ActionException {
 		FacadeType type = getFacadeType(req.getParameter("facadeType"));
 		
-		//call the appropriate action
-		switch(type) {
-			case PRODUCT_EXPLORER : 
-				loadProductExplorer(req);
-				break;
-			case GAP_ANALYSIS : 
-				loadGapAnalysis(req);
-				break;
-			default : break;
+		if(FacadeType.PRODUCT_EXPLORER.equals(type)){
+			configurePEData();
 		}
+		
+		//load action and execute retrieve
+		loadAction(type.getClassName()).retrieve(req);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.smt.sitebuilder.action.SBActionAdapter#build(com.siliconmtn.action.ActionRequest)
+	 */
+	@Override
+	public void build(ActionRequest req) throws ActionException{
+		FacadeType type = getFacadeType(req.getParameter("facadeType"));
+		
+		if(FacadeType.PRODUCT_EXPLORER.equals(type)){
+			configurePEData();
+		}
+		
+		//load action and execute build
+		loadAction(type.getClassName()).build(req);
 	}
 	
 	/**
-	 * Loads the product explorer as it would appear on public site
-	 * @param req
-	 * @throws ActionException
+	 * Configures data required to run Product Explorer for public site
 	 */
-	protected void loadProductExplorer(ActionRequest req) throws ActionException {
+	protected void configurePEData() {
 		//set proper action id to process solr request correctly for action
 		ModuleVO mod = (ModuleVO)attributes.get(Constants.MODULE_DATA);
 		String actionId = mod.getIntroText();
 		mod.setAttribute(ModuleVO.ATTRIBUTE_1, actionId);
 		setAttribute(Constants.MODULE_DATA, mod);
-				
-		ActionInterface ai = new ProductExplorer(actionInit);
-		ai.setDBConnection(dbConn);
-		ai.setAttributes(attributes);
-		ai.retrieve(req);
 	}
 	
 	/**
-	 * Loads the gap analysis as it would appear on public site
-	 * @param req
+	 * Dynamically instantiates the appropriate action class based on given class name
+	 * @param className - the fully qualified class name
+	 * @return - the corresponding action class
 	 * @throws ActionException
 	 */
-	protected void loadGapAnalysis(ActionRequest req) throws ActionException {
-		ActionInterface ai = new GapAnalysisAction(actionInit);
-		ai.setDBConnection(dbConn);
-		ai.setAttributes(attributes);
-		ai.retrieve(req);
+	private ActionInterface loadAction(String className) throws ActionException {
+		ActionInterface ai = null;
+		
+		//load the class and set attributes/dbConn
+		try {
+			Class<?> c = Class.forName(className);
+			ai = (ActionInterface) c.newInstance();
+			ai.setActionInit(actionInit);
+			ai.setAttributes(attributes);
+			ai.setDBConnection(dbConn);
+		} catch (Exception e) {
+			throw new ActionException("Could not instantiate tool facade class: ", e);
+		}
+		return ai;
 	}
 	
 	/**
