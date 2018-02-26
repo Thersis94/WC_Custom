333b9e3ce8cde951180aa97514912b55813818d4
James McKain <jmckain@siliconmtn.com>
2018-02-26 15:22:27 -0500

    refactor(Sc-453): Refactor code to use new classloader

--- src/com/biomed/smarttrak/admin/ToolsFacadeAction.java
+++ src/com/biomed/smarttrak/admin/ToolsFacadeAction.java
@@ -3,12 +3,15 @@ package com.biomed.smarttrak.admin;
 //WC Custom libs
 import com.biomed.smarttrak.action.GapAnalysisAction;
 import com.biomed.smarttrak.action.ProductExplorer;
+import com.siliconmtn.action.ActionControllerFactoryImpl;
+
 //Base libs
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
+
 //WebCrescendo libs
-import com.smt.sitebuilder.action.FacadeActionAdapter;
+import com.smt.sitebuilder.action.SimpleActionAdapter;
 import com.smt.sitebuilder.common.ModuleVO;
 import com.smt.sitebuilder.common.constants.Constants;
 
@@ -22,21 +25,20 @@ import com.smt.sitebuilder.common.constants.Constants;
  * @version 1.0
  * @since Jan 12, 2018
  ****************************************************************************/
-
-public class ToolsFacadeAction extends FacadeActionAdapter {
+public class ToolsFacadeAction extends SimpleActionAdapter {
 
 	public ToolsFacadeAction() {
 		super();
 	}
-	
+
 	public ToolsFacadeAction(ActionInitVO actionInit) {
 		super(actionInit);
 	} 
-	
-	protected enum FacadeType{
+
+	protected enum FacadeType {
 		PRODUCT_EXPLORER("explorer", ProductExplorer.class.getName()), 
 		GAP_ANALYSIS("analysis", GapAnalysisAction.class.getName());
-		
+
 		private String facadeTarget; 
 		private String className; //the fully qualified class name
 		private FacadeType(String facadeTarget, String className) {
@@ -44,14 +46,11 @@ public class ToolsFacadeAction extends FacadeActionAdapter {
 			this.className = className;
 		}
 		/*===Getters===*/
-		public String getFacadeTarget() {
-			return facadeTarget;
-		}
-		public String getClassName() {
-			return className;
-		}
+		public String getFacadeTarget() { return facadeTarget; }
+		public String getClassName() { return className; }
 	}
-	
+
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.smt.sitebuilder.action.SBActionAdapter#retrieve(com.siliconmtn.action.ActionRequest)
@@ -59,31 +58,31 @@ public class ToolsFacadeAction extends FacadeActionAdapter {
 	@Override
 	public void retrieve (ActionRequest req) throws ActionException {
 		FacadeType type = getFacadeType(req.getParameter("facadeType"));
-		
-		if(FacadeType.PRODUCT_EXPLORER.equals(type)){
+
+		if (FacadeType.PRODUCT_EXPLORER.equals(type))
 			configurePEData();
-		}
-			
+
 		//load action and execute retrieve
-		getActionInstance(type.getClassName()).retrieve(req);
+		ActionControllerFactoryImpl.loadAction(type.getClassName(), this).retrieve(req);
 	}
-	
+
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.smt.sitebuilder.action.SBActionAdapter#build(com.siliconmtn.action.ActionRequest)
 	 */
 	@Override
-	public void build(ActionRequest req) throws ActionException{
+	public void build(ActionRequest req) throws ActionException {
 		FacadeType type = getFacadeType(req.getParameter("facadeType"));
-		
-		if(FacadeType.PRODUCT_EXPLORER.equals(type)){
+
+		if (FacadeType.PRODUCT_EXPLORER.equals(type))
 			configurePEData();
-		}
-		
-		//load action and execute build
-		getActionInstance(type.getClassName()).build(req);
+
+		//load the action and execute build
+		ActionControllerFactoryImpl.loadAction(type.getClassName(), this).build(req);
 	}
-	
+
+
 	/**
 	 * Configures data required to run Product Explorer for public site
 	 */
@@ -94,7 +93,8 @@ public class ToolsFacadeAction extends FacadeActionAdapter {
 		mod.setAttribute(ModuleVO.ATTRIBUTE_1, actionId);
 		setAttribute(Constants.MODULE_DATA, mod);
 	}
-	
+
+
 	/**
 	 * Retrieves the facade type based on string passed
 	 * @param target
@@ -102,14 +102,11 @@ public class ToolsFacadeAction extends FacadeActionAdapter {
 	 * @throws ActionException
 	 */
 	private FacadeType getFacadeType(String target) throws ActionException {
-		FacadeType ft = null;
 		for (FacadeType type : FacadeType.values()) {
-			if(type.facadeTarget.equalsIgnoreCase(target)) {
-				ft = type;
-			}
+			if (type.getFacadeTarget().equalsIgnoreCase(target))
+				return type;
 		}
-		
-		if(ft == null) throw new ActionException("Unknown facade type. Please check value passed.");
-		return ft;
+		//no match, throw an exception
+		throw new ActionException("Unknown facade type. Please check value passed.");
 	}
 }
