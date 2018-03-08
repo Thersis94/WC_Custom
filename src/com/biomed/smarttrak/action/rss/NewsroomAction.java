a92dc698c2aa5d38e53b46179d4aa00a7d95df34
YetAnotherMask <edamschroder@siliconmtn.com>
2018-03-08 14:29:26 -0500

    refactor(Sc-783): Offload result limiting to dbprocessor.

--- src/com/biomed/smarttrak/action/rss/NewsroomAction.java
+++ src/com/biomed/smarttrak/action/rss/NewsroomAction.java
@@ -99,10 +99,9 @@ public class NewsroomAction extends SBActionAdapter {
 		}
 
 		DBProcessor dbp = new DBProcessor(dbConn, (String)getAttribute(Constants.CUSTOM_DB_SCHEMA));
-		List<RSSArticleVO> articles = dbp.executeSelect(loadArticleSql(hasStatus), vals, new RSSArticleVO());
-		int end = articles.size() < offset+10? articles.size() : offset+10;
-		if(!articles.isEmpty() && offset < articles.size())
-			this.putModuleData(articles.subList(offset, end), articles.size(), false);
+		List<RSSArticleVO> articles = dbp.executeSelect(loadArticleSql(hasStatus), vals, new RSSArticleVO(), null, offset, offset+10);
+		if(!articles.isEmpty())
+			this.putModuleData(articles, articles.size(), false);
 	}
 
 	/**
