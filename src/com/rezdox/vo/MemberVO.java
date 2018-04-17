6146b35ca6af4cd2dcff0935c376f04dd500134a
Tim Johnson <tjohnson@siliconmtn.com>
2018-04-17 16:50:07 -0600

    fix(REZDOX-151): cleanup various ui issues in business reviews

--- src/com/rezdox/vo/MemberVO.java
+++ src/com/rezdox/vo/MemberVO.java
@@ -33,6 +33,7 @@ public class MemberVO extends UserDataVO implements HumanNameIntfc, Serializable
 	private int statusFlg;
 	private int privacyFlg;
 	private String profilePicPath;
+	private String initials;
 	private Date createDate;
 
 	/**
@@ -210,6 +211,28 @@ public class MemberVO extends UserDataVO implements HumanNameIntfc, Serializable
 	}
 
 	/**
+	 * @return the initials
+	 */
+	public String getInitials() {
+		return initials;
+	}
+
+	/**
+	 * @param initials the initials to set
+	 */
+	public void setInitials(String initials) {
+		this.initials = initials;
+	}
+	
+	/**
+	 * Sets the member's initials based on first/last name in the VO
+	 */
+	private void setInitials() {
+		String name = StringUtil.checkVal(getFirstName()) + ' ' + StringUtil.checkVal(getLastName());
+		setInitials(StringUtil.abbreviate(name.trim(), 2).toUpperCase());
+	}
+
+	/**
 	 * Override for db processor to add profile_id to the member record
 	 */
 	@Override
@@ -226,6 +249,15 @@ public class MemberVO extends UserDataVO implements HumanNameIntfc, Serializable
 	public String getFirstName() {
 		return super.getFirstName();
 	}
+	
+	/* (non-Javadoc)
+	 * @see com.siliconmtn.security.UserDataVO#setFirstName(java.lang.String)
+	 */
+	@Override
+	public void setFirstName(String firstName) {
+		super.setFirstName(firstName);
+		setInitials();
+	}
 
 	/* (non-Javadoc)
 	 * @see com.siliconmtn.security.UserDataVO#getLastName()
@@ -234,6 +266,15 @@ public class MemberVO extends UserDataVO implements HumanNameIntfc, Serializable
 	@Column(name="last_nm")
 	public String getLastName() {
 		return super.getLastName();
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.siliconmtn.security.UserDataVO#setLastName(java.lang.String)
+	 */
+	@Override
+	public void setLastName(String lastName) {
+		super.setLastName(lastName);
+		setInitials();
 	}
 
 	/* (non-Javadoc)
