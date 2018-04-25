8689d208fceb20ca68cc74964bf7d763783e3d0a
James McKain <jmckain@siliconmtn.com>
2018-04-25 01:53:07 -0400

    fix(REZDOX-85): Integrate loading of notifications and project activity into the dashboard

--- src/com/rezdox/action/DashboardAction.java
+++ src/com/rezdox/action/DashboardAction.java
@@ -1,5 +1,6 @@
 package com.rezdox.action;
 
+import java.util.ArrayList;
 // JDK 1.8.x
 import java.util.Arrays;
 import java.util.HashMap;
@@ -11,7 +12,6 @@ import com.rezdox.vo.BusinessVO;
 // WC Custom Libs
 import com.rezdox.vo.MemberVO;
 import com.rezdox.vo.ResidenceVO;
-
 // SMT Base Libs
 import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
@@ -19,9 +19,10 @@ import com.siliconmtn.action.ActionRequest;
 import com.siliconmtn.data.GenericVO;
 import com.siliconmtn.db.DBUtil;
 import com.siliconmtn.db.orm.DBProcessor;
-
 // WC Libs
 import com.smt.sitebuilder.action.SimpleActionAdapter;
+import com.smt.sitebuilder.action.tools.NotificationLogVO;
+import com.smt.sitebuilder.common.ModuleVO;
 import com.smt.sitebuilder.common.constants.Constants;
 import com.smt.sitebuilder.security.SBUserRole;
 
@@ -45,10 +46,7 @@ public class DashboardAction extends SimpleActionAdapter {
 	private static final String BUS_REVIEW_TABLE = "rezdox_member_business_review ";
 	private static final String PROJECT_TABLE = "rezdox_project ";
 	private static final String WHERE_BUSINESS_ID = "where business_id = ? ";
-	
-	/**
-	 * 
-	 */
+
 	public DashboardAction() {
 		super();
 	}
@@ -66,23 +64,77 @@ public class DashboardAction extends SimpleActionAdapter {
 	 */
 	@Override
 	public void retrieve(ActionRequest req) throws ActionException {
-		
-		SBUserRole role = ((SBUserRole)req.getSession().getAttribute(Constants.ROLE_DATA));
+		SBUserRole role = (SBUserRole) req.getSession().getAttribute(Constants.ROLE_DATA);
 		MemberVO member = (MemberVO) req.getSession().getAttribute(Constants.USER_DATA);
-		String schema = (String)attributes.get(Constants.CUSTOM_DB_SCHEMA);
-		
-		// Retrieve the business or residence data based upon the user role
-		if (RezDoxUtils.REZDOX_RESIDENCE_ROLE.equalsIgnoreCase(role.getRoleId())) {
-			this.setModuleData(getResidenceData(member, schema));
-		} else if (RezDoxUtils.REZDOX_BUSINESS_ROLE.equalsIgnoreCase(role.getRoleId())) { 
-			this.setModuleData(getBusinessData(member, schema));
-		} else { 
-			Map<String, Object> allData = getBusinessData(member, schema);
+		String schema = getCustomSchema();
+		Map<String, Object> allData = new HashMap<>();
+		boolean isBusinessOnlyRole = RezDoxUtils.REZDOX_BUSINESS_ROLE.equalsIgnoreCase(role.getRoleId());
+		boolean isResidenceOnlyRole = RezDoxUtils.REZDOX_RESIDENCE_ROLE.equalsIgnoreCase(role.getRoleId());
+
+		//not strictly a business user?  Then you'll need residence data loaded
+		if (!isBusinessOnlyRole)
 			allData.putAll(getResidenceData(member, schema));
-			this.setModuleData(allData);
-		}
+
+		//not strictly a residence user?  Then you'll need business data loaded
+		if (!isResidenceOnlyRole)
+			allData.putAll(getBusinessData(member, schema));
+
+		allData.put("notifications", loadMyNotifications(req));
+		allData.put("projecthistory", loadMyRecentProjects(member, isBusinessOnlyRole, isResidenceOnlyRole));
+
+		setModuleData(allData);
 	}
-	
+
+
+	/**
+	 * Loads a list of notifications for the logged-in user 
+	 * @param profileId
+	 * @return
+	 * @throws ActionException 
+	 */
+	@SuppressWarnings("unchecked")
+	private List<NotificationLogVO> loadMyNotifications(ActionRequest req) throws ActionException {
+		MyNotificationsAction action = new MyNotificationsAction(getDBConnection(), getAttributes());
+		action.retrieve(req);
+		ModuleVO mod = (ModuleVO) action.getAttribute(Constants.MODULE_DATA);
+		return (List<NotificationLogVO> ) mod.getActionData();
+	}
+
+
+	/**
+	 * Loads a list of home-history or project-history for the logged-in user 
+	 * @param profileId
+	 * @return
+	 * @throws ActionException 
+	 */
+	@SuppressWarnings("unchecked")
+	private List<GenericVO> loadMyRecentProjects(MemberVO member, boolean isBusinessOnlyRole, boolean isResidenceOnlyRole) {
+		List<Object> params = new ArrayList<>();
+		String schema = getCustomSchema();
+		StringBuilder sql = new StringBuilder(400);
+		if (!isBusinessOnlyRole) {
+			sql.append("select 'SMT1'+a.project_id as key, a.project_nm as value, coalesce(a.update_dt, a.create_dt) from ").append(schema).append("REZDOX_PROJECT a ");
+			sql.append(DBUtil.INNER_JOIN).append(schema).append("REZDOX_RESIDENCE r on a.residence_id=r.residence_id ");
+			sql.append(DBUtil.INNER_JOIN).append(schema).append("REZDOX_RESIDENCE_MEMBER_XR rm on r.residence_id=rm.residence_id and rm.member_id=? and rm.status_flg=1 ");
+			sql.append("where a.residence_view_flg != 0 ");
+			params.add(member.getMemberId());
+		}
+		if (!isResidenceOnlyRole) {
+			if (!isBusinessOnlyRole) sql.append(DBUtil.UNION); //put a union if this is the 2nd half of the query
+			sql.append("select 'SMT2'+ a.project_id as key, a.project_nm as value, coalesce(a.update_dt, a.create_dt) from ").append(schema).append("REZDOX_PROJECT a ");
+			sql.append(DBUtil.INNER_JOIN).append(schema).append("REZDOX_BUSINESS b on a.business_id=b.business_id ");
+			sql.append(DBUtil.INNER_JOIN).append(schema).append("REZDOX_BUSINESS_MEMBER_XR bm on b.business_id=bm.business_id and bm.member_id=? and bm.status_flg=1 ");
+			sql.append("where a.business_view_flg != 0 ");
+			params.add(member.getMemberId());
+		}
+		sql.append("order by 3 desc"); //most recent first, homogenized
+		log.debug(sql);
+
+		DBProcessor dbp = new DBProcessor(getDBConnection(), schema);
+		return dbp.executeSelect(sql.toString(), params, new GenericVO());
+	}
+
+
 	/**
 	 * Retrieve the business dashboard data
 	 * @param member
@@ -94,10 +146,10 @@ public class DashboardAction extends SimpleActionAdapter {
 		data.put("rewards", getRewardsInfo(member.getMemberId(), schema));
 		data.put("connections", getConnections(member.getMemberId(), schema));
 		data.put("business", getBusinessInfo(member.getMemberId(), schema));
-		
+
 		return data;
 	}
-	
+
 	/**
 	 * Gets the summary attributes for the business
 	 * @param bus
@@ -130,13 +182,13 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append(DBUtil.FROM_CLAUSE).append(schema).append(PROJECT_TABLE);
 		sql.append(WHERE_BUSINESS_ID).append(RECENT_SQL).append(BUS_ID_GROUP);
 		sql.append("order by slug_txt ");
-		
+
 		// Add the attributes to the business
 		DBProcessor db = new DBProcessor(getDBConnection());
 		List<Object> vals = Arrays.asList(bId,bId,bId,bId,bId,bId);
 		bus.addAttributes(db.executeSelect(sql.toString(), vals, new BusinessAttributeVO()));
 	}
-	
+
 	/**
 	 * Gets a list of businesses assigned to the member
 	 * @param memberId
@@ -149,13 +201,13 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append(DBUtil.FROM_CLAUSE).append(schema).append("rezdox_business_member_xr a ");  
 		sql.append(DBUtil.INNER_JOIN).append(schema).append("rezdox_business b on a.business_id = b.business_id "); 
 		sql.append("where member_id = ? order by business_nm");
-		
+
 		DBProcessor db = new DBProcessor(getDBConnection());
 		List<BusinessVO> businesses = db.executeSelect(sql.toString(), Arrays.asList(memberId), new BusinessVO());
 		for(BusinessVO business : businesses) getBusinessAttributes(business, schema);
 		return businesses;
 	}
-	
+
 	/**
 	 * Retrieve the data for the residence dashboard
 	 * @param member
@@ -163,16 +215,16 @@ public class DashboardAction extends SimpleActionAdapter {
 	 * @return
 	 */
 	protected Map<String, Object> getResidenceData(MemberVO member, String schema) {
-		
+
 		Map<String, Object> data = new HashMap<>();
 		data.put("residence", getResidenceInfo(member.getMemberId(), schema));
 		data.put("rewards", getRewardsInfo(member.getMemberId(), schema));
 		data.put("connections", getConnections(member.getMemberId(), schema));
 		data.put("reviews", getReviews(member.getMemberId(), schema));
-		
+
 		return data;
 	}
-	
+
 	/**
 	 * retrieves the number of connections assigned to a member
 	 * @param memberId
@@ -185,12 +237,12 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append("where (sndr_member_id = ? ");
 		sql.append("or rcpt_member_id = ? ) ");
 		sql.append("and approved_flg = 1 ");
-		
+
 		DBProcessor db = new DBProcessor(getDBConnection());
 		List<GenericVO> data = db.executeSelect(sql.toString(), Arrays.asList(memberId, memberId), new GenericVO());
 		return data.isEmpty() ? new GenericVO(0,0) : data.get(0);
 	}
-	
+
 	/**
 	 * Retrieves review summary information
 	 * @param memberId
@@ -202,12 +254,12 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append("select count(*) as key, coalesce(sum(rating_no), 0) as value ");
 		sql.append(DBUtil.FROM_CLAUSE).append(schema).append(BUS_REVIEW_TABLE);
 		sql.append("where member_id = ? and parent_id is null ");
-		
+
 		DBProcessor db = new DBProcessor(getDBConnection());
 		List<GenericVO> data = db.executeSelect(sql.toString(), Arrays.asList(memberId), new GenericVO());
 		return data.isEmpty() ? new GenericVO(12,3.3) : data.get(0);
 	}
-	
+
 	/**
 	 * Retrieves the summarized residence data for each location for a given member
 	 * @param memberId
@@ -231,11 +283,11 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append("group by a.residence_id ");
 		sql.append(") as p on c.residence_id = p.residence_id ");
 		sql.append("where b.member_id = ? ");
-		
+
 		DBProcessor db = new DBProcessor(getDBConnection());
 		return db.executeSelect(sql.toString(), Arrays.asList(memberId, memberId), new ResidenceVO(), "residence_id");
 	}
-	
+
 	/**
 	 * Gets the current number of rewards and the number earned in last 90 days
 	 * @param memberId
@@ -256,10 +308,10 @@ public class DashboardAction extends SimpleActionAdapter {
 		sql.append(") as b on a.member_id = b.member_id ");
 		sql.append("where a.member_id = ? ");
 		sql.append("group by a.member_id, recent_rewards_total ");
-		
+
 		DBProcessor db = new DBProcessor(getDBConnection());
 		List<GenericVO> rewards = db.executeSelect(sql.toString(), Arrays.asList(memberId, memberId), new GenericVO());
-		
+
 		if (! rewards.isEmpty()) return rewards.get(0);
 		return new GenericVO(0,0);
 	}
