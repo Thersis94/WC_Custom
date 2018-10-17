0708602dfae4000d72c80cbef610cc6438adcb1c
billysmt <billy@siliconmtn.com>
2018-10-17 15:53:09 -0400

    refactor(SRT): Cleaned up Roster Import WF Code.

--- src/com/depuysynthes/srt/data/SRTUserImportModule.java
+++ src/com/depuysynthes/srt/data/SRTUserImportModule.java
@@ -14,21 +14,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.depuysynthes.srt.SRTRosterAction;
 import com.depuysynthes.srt.SRTRosterImportAction;
 import com.depuysynthes.srt.util.SRTRosterImporter;
 import com.depuysynthes.srt.util.SRTUtil;
 import com.depuysynthes.srt.vo.SRTRosterVO;
+import com.siliconmtn.action.ActionException;
 import com.siliconmtn.db.DBUtil;
 import com.siliconmtn.db.orm.DBProcessor;
 import com.siliconmtn.exception.DatabaseException;
 import com.siliconmtn.security.EncryptionException;
 import com.siliconmtn.security.StringEncrypter;
+import com.siliconmtn.security.UserDataVO;
 import com.siliconmtn.util.Convert;
 import com.siliconmtn.util.StringUtil;
 import com.siliconmtn.workflow.data.WorkflowModuleVO;
 import com.siliconmtn.workflow.modules.AbstractWorkflowModule;
-import com.smt.sitebuilder.action.user.ProfileManager;
 import com.smt.sitebuilder.action.user.ProfileManagerFactory;
+import com.smt.sitebuilder.action.user.SBProfileManager;
+import com.smt.sitebuilder.admin.action.OrganizationAction;
+import com.smt.sitebuilder.admin.action.SiteAction;
 import com.smt.sitebuilder.common.constants.Constants;
 import com.smt.sitebuilder.util.WCConfigUtil;
 
@@ -46,8 +51,6 @@ import com.smt.sitebuilder.util.WCConfigUtil;
 public class SRTUserImportModule extends AbstractWorkflowModule {
 
 	public static final String SALES_ROSTER_ID = "8";
-	private static final String DEACTIVATIONS = "DEACTIVATIONS";
-	private static final String UPDATES = "UPDATES";
 	public static final String TEMP_TABLE_NM = "tempTableNm";
 	public static final String FILE_NM = "fileNm";
 
@@ -64,7 +67,6 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 	/* (non-Javadoc)
 	 * @see com.siliconmtn.workflow.modules.AbstractWorkflowModule#run()
 	 */
-	@SuppressWarnings("unchecked")
 	@Override
 	protected void run() throws Exception {
 		String tempTableName = (String) mod.getConfig(TEMP_TABLE_NM).getValue();
@@ -80,20 +82,16 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 		log.debug("Existing Users count: "+ existingUsers.size());
 
 		//Get profileIds of the deactivatedUsers
-		Map<String, Object> data = getDeactivations(tempData, existingUsers);
-		log.debug("Deactivated Users count: "+ ((List<String>) data.get(DEACTIVATIONS)).size());
-		log.debug("Updated Users count: "+ ((List<String>) data.get(UPDATES)).size());
-		log.debug("New Users count: "+ tempData.size());
+		List<String> deactivations = getDeactivations(tempData, existingUsers);
+		log.debug("Deactivated Users count: "+ deactivations.size());
+		log.debug("New/Update Users count: "+ tempData.size());
 		log.debug("Finished loading Data");
 		
 		//Deactivate Current Sales Rosters not in tempDatad
-		deactivateSalesRosters((List<String>) data.get(DEACTIVATIONS), opCoId);
-
-		//Update Existing Records with New Data.
-		updateExisting((List<SRTRosterVO>)data.get(UPDATES), opCoId);
+		deactivateSalesRosters(deactivations, opCoId);
 
 		//Activate New Users
-		activateNewUsers(tempData.values());
+		updateUsers(tempData.values(), opCoId);
 
 		//DropTempTable
 		dropTempTable(tempTableName);
@@ -126,76 +124,33 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 	}
 
 	/**
-	 * Updates Existing Users in the System.
-	 * @param list
-	 * @throws SQLException 
-	 * @throws DatabaseException 
-	 * @throws com.siliconmtn.db.util.DatabaseException 
-	 */
-	private void updateExisting(List<SRTRosterVO> updateUserData, String opCoId) throws DatabaseException, SQLException, com.siliconmtn.db.util.DatabaseException {
-		//Fast Fail if nothing to do.
-		if(StringUtil.isEmpty(opCoId) || updateUserData == null || updateUserData.isEmpty()) {
-			return;
-		}
-
-		Map<String, List<Object>> psValues = new HashMap<>();
-		List<Object> data;
-		ProfileManager pm = ProfileManagerFactory.getInstance(attributes);
-		for(SRTRosterVO user : updateUserData) {
-			user.setProfileId(pm.checkProfile(user, getConnection()));
-			pm.updateProfile(user, getConnection());
-			data = new ArrayList<>();
-			data.add(user.getAccountNo());
-			data.add(user.getWorkgroupId());
-			data.add(user.getWwid());
-			data.add(user.getTerritoryId());
-			data.add(user.getArea());
-			data.add(user.getRegion());
-			data.add(user.getEngineeringContact());
-			data.add(user.getIsAdmin());
-			data.add(1);
-			data.add(Convert.getCurrentTimestamp());
-			data.add(null);
-			data.add(user.getProfileId());
-			data.add(opCoId);
-			psValues.put(user.getProfileId(), data);
-		}
-
-		new DBProcessor(getConnection(), (String)attributes.get(Constants.CUSTOM_DB_SCHEMA)).executeBatch(updateExistingSql(), psValues);
-	}
-
-	/**
-	 * Builds Update Sql query.
-	 * @return
-	 */
-	private String updateExistingSql() {
-		StringBuilder sql = new StringBuilder(500);
-		sql.append(DBUtil.UPDATE_CLAUSE).append(attributes.get(Constants.CUSTOM_DB_SCHEMA));
-		sql.append("DPY_SYN_SRT_ROSTER set ACCOUNT_NO = ?, workgroup_id = ?, ");
-		sql.append("wwid = ?, territory_id = ?, area = ?, region = ?, ");
-		sql.append("engineering_contact = ?, is_admin = ?, is_active = ?, update_dt = ?, ");
-		sql.append("deactivated_dt = ? ");
-		sql.append(DBUtil.WHERE_CLAUSE).append("profile_id = ? and op_co_id = ?");
-		return sql.toString();
-	}
-
-	/**
 	 * Call the SRTUserImport System with the remaining SRTRosterVOs
+	 * @param opCoId 
 	 * @param tempData
 	 * @throws Exception 
 	 */
-	private void activateNewUsers(Collection<SRTRosterVO> newRosters) throws Exception {
+	private void updateUsers(Collection<SRTRosterVO> newRosters, String opCoId) throws Exception {
 
 		//Fast Fail if no new Users.
 		if(newRosters == null || newRosters.isEmpty()) {
 			return;
 		}
 
+		//Clone Attributes Map
+		Map<String, Object> config = new HashMap<>();
+		config.putAll(attributes);
+
 		//Load all SiteConfig from DB
-		attributes.putAll(WCConfigUtil.getSiteConfig(getConnection(), SRTUtil.PUBLIC_SITE_ID));
+		config.putAll(WCConfigUtil.getSiteConfig(getConnection(), SRTUtil.PUBLIC_SITE_ID));
+
+		//Load Config from Constants.
+		config.put(OrganizationAction.ORGANIZATION_ID, SRTUtil.SRT_ORG_ID);
+		config.put(SiteAction.SITE_ID, SRTUtil.PUBLIC_SITE_ID);
+		config.put("registerActionId", SRTUtil.REGISTRATION_GRP_ID);
+		config.put(SRTUtil.OP_CO_ID, opCoId);
 
 		//Create new Importer with updated attributes map.
-		SRTRosterImporter importer = new SRTRosterImporter(getConnection(), attributes);
+		SRTRosterImporter importer = new SRTRosterImporter(getConnection(), config);
 
 		//Perform Import with newRosters.
 		importer.importUsers(new ArrayList<>(newRosters));
@@ -211,6 +166,7 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 		if(StringUtil.isEmpty(opCoId) || deactivations == null || deactivations.isEmpty()) {
 			return;
 		}
+
 		try(PreparedStatement ps = getConnection().prepareStatement(getDeactivationSql(deactivations.size()))) {
 			int i = 1;
 			ps.setInt(i++, 0);
@@ -223,6 +179,37 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 			int res = ps.executeUpdate();
 			log.debug("Deactivated " + res + " users");
 		}
+
+		SRTRosterAction sra = new SRTRosterAction();
+		sra.setDBConnection(this.getConnection());
+		sra.setAttributes(attributes);
+		try {
+			List<SRTRosterVO> rosters = convertDeactivations(deactivations);
+			for(SRTRosterVO roster: rosters) {
+				sra.saveProfileRole(SRTUtil.PUBLIC_SITE_ID, roster, true);
+			}
+		} catch (ActionException | DatabaseException e) {
+			log.error("Error Deactivating Users", e);
+		}
+	}
+
+	/**
+	 * Load Profiles for Deactivated Users.
+	 * @param deactivations
+	 * @return
+	 * @throws SQLException 
+	 * @throws DatabaseException 
+	 */
+	private List<SRTRosterVO> convertDeactivations(List<String> deactivations) throws DatabaseException, SQLException {
+		SBProfileManager pm = (SBProfileManager) ProfileManagerFactory.getInstance(attributes);
+		List<UserDataVO> users = pm.searchProfile(getConnection(), deactivations);
+		List<SRTRosterVO> rosters = new ArrayList<>();
+		for(UserDataVO u : users) {
+			SRTRosterVO r = new SRTRosterVO();
+			r.setData(u.getDataMap());
+			rosters.add(r);
+		}
+		return rosters;
 	}
 
 	/**
@@ -233,7 +220,7 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 	private String getDeactivationSql(int profileIdCount) {
 		StringBuilder sql = new StringBuilder(200 + profileIdCount * 3);
 		sql.append(DBUtil.UPDATE_CLAUSE).append(schema).append("DPY_SYN_SRT_ROSTER ");
-		sql.append("set is_active = ?, deactivated_dt = ? where op_co_id = ? and profile_id in (");
+		sql.append("set is_active = ?, deactivated_dt = ?, wwid = null where op_co_id = ? and profile_id in (");
 		DBUtil.preparedStatmentQuestion(profileIdCount, sql);
 		sql.append(")");
 		return sql.toString();
@@ -245,23 +232,15 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 	 * @param existingUsers
 	 * @return
 	 */
-	private Map<String, Object> getDeactivations(Map<String, SRTRosterVO> tempData, Map<String, String> existingUsers) {
-		Map<String, Object> data = new HashMap<>();
+	private List<String> getDeactivations(Map<String, SRTRosterVO> tempData, Map<String, String> existingUsers) {
 		List<String> deactivations = new ArrayList<>();
-		List<SRTRosterVO> updates = new ArrayList<>();
 		for(Entry<String, String> e : existingUsers.entrySet()) {
 			SRTRosterVO roster = tempData.get(e.getKey());
 			if(roster == null) {
 				deactivations.add(e.getValue());
-			} else {
-				roster.setProfileId(e.getValue());
-				updates.add(roster);
-				tempData.remove(e.getKey());
 			}
 		}
-		data.put(DEACTIVATIONS, deactivations);
-		data.put(UPDATES, updates);
-		return data;
+		return deactivations;
 	}
 
 	/**
@@ -275,6 +254,7 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 		try(PreparedStatement ps = getConnection().prepareStatement(loadExistingDataSql())) {
 			ps.setString(1, opCoId);
 			ps.setString(2, SALES_ROSTER_ID);
+			ps.setInt(3, 1);
 
 			ResultSet rs = ps.executeQuery();
 			StringEncrypter se = new StringEncrypter((String)attributes.get(Constants.ENCRYPT_KEY));
@@ -298,7 +278,8 @@ public class SRTUserImportModule extends AbstractWorkflowModule {
 		sql.append("select p.profile_id, p.email_address_txt from profile p ");
 		sql.append(DBUtil.INNER_JOIN).append(schema).append("dpy_syn_srt_roster r ");
 		sql.append("on p.profile_id = r.profile_id ");
-		sql.append(DBUtil.WHERE_CLAUSE).append("r.op_co_id = ? and r.workgroup_id = ?");
+		sql.append(DBUtil.WHERE_CLAUSE).append("r.op_co_id = ? and r.workgroup_id = ? ");
+		sql.append("and r.is_active = ?");
 		return sql.toString();
 	}
 
