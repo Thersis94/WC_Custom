b4c50c7647893abafd2acfe2680e1f48a8fad2cb
Tim Johnson <tjohnson@siliconmtn.com>
2018-11-28 15:47:34 -0700

    fix(TicketNextSteps): add approval code to TicketDataVO

--- src/com/wsla/data/ticket/ApprovalCode.java
+++ src/com/wsla/data/ticket/ApprovalCode.java
@@ -12,12 +12,12 @@ package com.wsla.data.ticket;
  * @since Nov 27, 2018
  * @updates:
  ****************************************************************************/
-public enum ApprovalCodes {
+public enum ApprovalCode {
 	UNKNOWN(false,-1), APPROVED(false, 0), REJECTED(true, 1);
 	boolean rejected;
 	int level;
 	
-	private ApprovalCodes(boolean rejected, int level) {
+	private ApprovalCode(boolean rejected, int level) {
         this.level = level;
         this.rejected = rejected;
     }
