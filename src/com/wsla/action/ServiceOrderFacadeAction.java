42eef9577ff239c2b45ecbd5c0052f1b818c1c60
Tim Johnson <tjohnson@siliconmtn.com>
2018-12-12 16:27:04 -0700

    fix(TicketNextSteps): update serial number from equipment modal

--- src/com/wsla/action/ServiceOrderFacadeAction.java
+++ src/com/wsla/action/ServiceOrderFacadeAction.java
@@ -33,7 +33,7 @@ public class ServiceOrderFacadeAction extends FacadeActionAdapter {
 	/**
 	 * Default type if not passed
 	 */
-	public static final String DEFAULT_TYPE = "overview";
+	public static final String DEFAULT_TYPE = "serviceOrder";
 
 	/**
 	 * Request key utilized top determine widget to call
