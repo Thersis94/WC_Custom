d35687d851d02758919e8f7bde21f656f506288a
YetAnotherMask <edamschroder@siliconmtn.com>
2018-12-19 17:38:26 -0500

    fix(Sc-1210): Update validator.

--- src/com/biomed/smarttrak/security/SmartTRAKUserValidator.java
+++ src/com/biomed/smarttrak/security/SmartTRAKUserValidator.java
@@ -2,9 +2,9 @@ package com.biomed.smarttrak.security;
 
 import com.biomed.smarttrak.vo.UserVO;
 import com.siliconmtn.http.SMTServletRequest;
-import com.siliconmtn.util.Convert;
 import com.siliconmtn.util.StringUtil;
 import com.smt.sitebuilder.common.constants.Constants;
+import com.smt.sitebuilder.http.WCUtilityFilter;
 import com.smt.sitebuilder.security.UserValidatorInterface;
 
 
@@ -23,57 +23,34 @@ import com.smt.sitebuilder.security.UserValidatorInterface;
 
 public class SmartTRAKUserValidator implements UserValidatorInterface {
 	private static final String ACCOUNT_PAGE = "/my-account";
-	private static final String PAGE_QS = "?msg=User%20Profile%20Icomplete.%20Please%20Complete%20User%20Profile.";
-	private static final String SITE_LOCK = "siteLock";
+	private static final String PAGE_QS = "?msg=User%20Profile%20Incomplete.%20Please%20Complete%20User%20Profile.";
 	private static final String INIT_DEST = "initialDestination";
-
-	@Override
-	public boolean validateUser(SMTServletRequest req) {
-		if (Convert.formatBoolean(req.getSession().getAttribute(SITE_LOCK)) &&
-				ACCOUNT_PAGE.equals(StringUtil.checkVal(req.getRequestURI()).replace(req.getContextPath(), ""))) return true;
-		
-		return isValidUser(req);
-	}
-
-	@Override
-	public void userApproved(SMTServletRequest req) {
-		req.getSession().removeAttribute(SITE_LOCK);
-		String initDest = (String)req.getSession().getAttribute(INIT_DEST);
-		if (!StringUtil.isEmpty(initDest)
-				&& isValidUser(req)) {
-			req.setAttribute(Constants.REDIRECT_REQUEST, Boolean.TRUE);
-			req.setAttribute(Constants.REDIRECT_URL, initDest);
-			req.getSession().removeAttribute(INIT_DEST);
-		}
-	}
-
 	
-	/**
-	 * Check to see if the supplied user has all fields set properly
-	 * @param req
-	 * @return
-	 */
-	private boolean isValidUser(SMTServletRequest req) {
+	
+	@Override
+	public String approveUser(SMTServletRequest req) {
+		String initDest = (String)req.getSession().getAttribute(INIT_DEST);
 		UserVO smarttrakUser = (UserVO) req.getSession().getAttribute(Constants.USER_DATA);
-		if (smarttrakUser == null) return true;
-		if (StringUtil.isEmpty(smarttrakUser.getFirstName())) return false;
-		if (StringUtil.isEmpty(smarttrakUser.getLastName())) return false;
-		if (StringUtil.isEmpty(smarttrakUser.getEmailAddress())) return false;
-		if (StringUtil.isEmpty(smarttrakUser.getMainPhone())) return false;
-		if (StringUtil.isEmpty(smarttrakUser.getTitle())) return false;
-		if (smarttrakUser.getDivisions() == null)return false;
-		
-		return true;
+		if (!StringUtil.isEmpty(initDest) && smarttrakUser.isValidProfile()) {
+			return initDest;
+		}
+		return "";
 	}
+	
 
 	@Override
-	public void userRejected(SMTServletRequest req) {
-		req.getSession().setAttribute(SITE_LOCK, true);
-		if (StringUtil.isEmpty((String) req.getSession().getAttribute(INIT_DEST)))
+	public String rejectUser(SMTServletRequest req) {
+		if (StringUtil.isEmpty((String) req.getSession().getAttribute(INIT_DEST))) {
 			req.getSession().setAttribute(INIT_DEST, StringUtil.checkVal(req.getRequestURI()).replace(req.getContextPath(), "") + req.getCompleteQueryString());
-		
-		req.setAttribute(Constants.REDIRECT_REQUEST, Boolean.TRUE);
-		req.setAttribute(Constants.REDIRECT_URL, ACCOUNT_PAGE + PAGE_QS);
+			req.getSession().setAttribute(WCUtilityFilter.APPROVAL_REDIRECT, true);
+		}
+		return ACCOUNT_PAGE + PAGE_QS;
+	}
+	
+
+	@Override
+	public boolean isValidRequest(SMTServletRequest req) {
+		return ACCOUNT_PAGE.equals(StringUtil.checkVal(req.getRequestURI()).replace(req.getContextPath(), ""));
 	}
 
 }
