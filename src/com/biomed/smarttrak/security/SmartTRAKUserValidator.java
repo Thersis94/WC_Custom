483a200e00bc896d1c02366d47fbb7c1313b936f
YetAnotherMask <edamschroder@siliconmtn.com>
2018-12-20 14:46:34 -0500

    fix(Sc-1210): Mirror changes to the validator.

--- src/com/biomed/smarttrak/security/SmartTRAKUserValidator.java
+++ src/com/biomed/smarttrak/security/SmartTRAKUserValidator.java
@@ -4,7 +4,6 @@ import com.biomed.smarttrak.vo.UserVO;
 import com.siliconmtn.http.SMTServletRequest;
 import com.siliconmtn.util.StringUtil;
 import com.smt.sitebuilder.common.constants.Constants;
-import com.smt.sitebuilder.http.WCUtilityFilter;
 import com.smt.sitebuilder.security.UserValidatorInterface;
 
 
@@ -26,25 +25,24 @@ public class SmartTRAKUserValidator implements UserValidatorInterface {
 	private static final String PAGE_QS = "?msg=User%20Profile%20Incomplete.%20Please%20Complete%20User%20Profile.";
 	private static final String INIT_DEST = "initialDestination";
 	
-	
-	@Override
-	public String approveUser(SMTServletRequest req) {
-		String initDest = (String)req.getSession().getAttribute(INIT_DEST);
-		UserVO smarttrakUser = (UserVO) req.getSession().getAttribute(Constants.USER_DATA);
-		if (!StringUtil.isEmpty(initDest) && smarttrakUser.isValidProfile()) {
-			return initDest;
-		}
-		return "";
-	}
-	
 
 	@Override
-	public String rejectUser(SMTServletRequest req) {
-		if (StringUtil.isEmpty((String) req.getSession().getAttribute(INIT_DEST))) {
-			req.getSession().setAttribute(INIT_DEST, StringUtil.checkVal(req.getRequestURI()).replace(req.getContextPath(), "") + req.getCompleteQueryString());
-			req.getSession().setAttribute(WCUtilityFilter.APPROVAL_REDIRECT, true);
+	public String getCorrectivePage(SMTServletRequest req, boolean isValidUser) {
+		String destUrl = "";
+		
+		if (isValidUser) {
+			String initDest = (String)req.getSession().getAttribute(INIT_DEST);
+			UserVO smarttrakUser = (UserVO) req.getSession().getAttribute(Constants.USER_DATA);
+			
+			destUrl = !StringUtil.isEmpty(initDest) && smarttrakUser.isValidProfile()? initDest : "";
+		} else {
+			if (StringUtil.isEmpty((String) req.getSession().getAttribute(INIT_DEST))) {
+				req.getSession().setAttribute(INIT_DEST, StringUtil.checkVal(req.getRequestURI()).replace(req.getContextPath(), "") + req.getCompleteQueryString());
+			}
+			destUrl = ACCOUNT_PAGE + PAGE_QS;
 		}
-		return ACCOUNT_PAGE + PAGE_QS;
+		
+		return destUrl;
 	}
 	
 
