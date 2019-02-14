1c9c02287701d2ffa6f8d84d5579e1d60ec444c7
James Camire <james@siliconmtn.com>
2019-02-13 20:17:22 -0700

    Added code to the attribute widget

--- src/com/restpeer/action/admin/AttributeWidget.java
+++ src/com/restpeer/action/admin/AttributeWidget.java
@@ -1,9 +1,19 @@
 package com.restpeer.action.admin;
 
-import com.siliconmtn.action.ActionException;
+// JDK 1.8.x
+import java.util.ArrayList;
+import java.util.List;
+
+// RP Libs
+import com.restpeer.data.AttributeVO;
+
 // SMT Base Libs
+import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
+import com.siliconmtn.db.orm.DBProcessor;
+import com.siliconmtn.util.StringUtil;
+
 // WC Libs
 import com.smt.sitebuilder.action.SBActionAdapter;
 
@@ -47,7 +57,34 @@ public class AttributeWidget extends SBActionAdapter {
 	 */
 	@Override
 	public void retrieve(ActionRequest req) throws ActionException {
+		putModuleData(getAttributeValues(req.getParameter("parentCode")));
+	}
+	
+	/**
+	 * 
+	 * @param parentCode
+	 * @return
+	 */
+	public List<AttributeVO> getAttributeValues(String parentCode) {
+		StringBuilder sql = new StringBuilder(128);
+		sql.append("select * from ").append(getCustomSchema()).append("rp_attribute a ");
+		sql.append("inner join ").append(getCustomSchema()).append("rp_category b ");
+		sql.append("on a.category_cd = b.category_cd ");
+		sql.append("where 1=1 ");
 		
+		List<Object> vals = new ArrayList<>();
+		if (!StringUtil.isEmpty(parentCode)) {
+			sql.append("and parent_cd = ? ");
+			vals.add(parentCode);
+		} else {
+			sql.append("and parent_cd is null ");
+		}
+		
+		sql.append("order by attribute_nm");
+		
+		// Execute and return the data
+		DBProcessor db = new DBProcessor(getDBConnection());
+		return db.executeSelect(sql.toString(), vals, new AttributeVO());
 	}
 	
 	/*
@@ -56,7 +93,16 @@ public class AttributeWidget extends SBActionAdapter {
 	 */
 	@Override
 	public void build(ActionRequest req) throws ActionException {
+		AttributeVO attr = new AttributeVO(req);
+		DBProcessor db = new DBProcessor(getDBConnection(), getCustomSchema());
 		
+		try {
+			db.save(attr);
+			setModuleData(attr);
+		} catch (Exception e) {
+			log.error("Unable to save attr: " + attr, e);
+			setModuleData(attr, 0, e.getLocalizedMessage());
+		}
 	}
 }
 
