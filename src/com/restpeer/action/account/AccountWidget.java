f30113aea76d41b8b189e1dd64d36b1ca9be0aa0
James Camire <james@siliconmtn.com>
2019-02-16 00:34:43 -0700

    started working ont he account screens

---
+++ src/com/restpeer/action/account/AccountWidget.java
@@ -0,0 +1,37 @@
+package com.restpeer.action.account;
+
+//SMT Base Libs
+import com.siliconmtn.action.ActionInitVO;
+
+// WC Libs
+import com.smt.sitebuilder.action.SimpleActionAdapter;
+
+/****************************************************************************
+ * <b>Title</b>: AccountWidget.java
+ * <b>Project</b>: WC_Custom
+ * <b>Description: </b> Manages the display
+ * <b>Copyright:</b> Copyright (c) 2019
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Feb 13, 2019
+ * @updates:
+ ****************************************************************************/
+public class AccountWidget extends SimpleActionAdapter {
+
+	/**
+	 * 
+	 */
+	public AccountWidget() {
+		super();
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public AccountWidget(ActionInitVO arg0) {
+		super(arg0);
+	}
+
+}
