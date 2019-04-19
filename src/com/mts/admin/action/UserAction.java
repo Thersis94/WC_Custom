8feeab89f7d044c43f8e09722c15a7446593f2b0
James Camire <james@siliconmtn.com>
2019-04-19 00:54:10 -0600

    feat(Users): Added author and user management

--- src/com/mts/admin/action/UserAction.java
+++ src/com/mts/admin/action/UserAction.java
@@ -18,8 +18,11 @@ import com.siliconmtn.db.DBUtil;
 import com.siliconmtn.db.orm.*;
 import com.siliconmtn.db.pool.SMTDBConnection;
 import com.siliconmtn.db.util.DatabaseException;
+import com.siliconmtn.util.StringUtil;
 //WC Libs
 import com.smt.sitebuilder.action.user.UserBaseWidget;
+import com.smt.sitebuilder.common.SiteVO;
+import com.smt.sitebuilder.common.constants.Constants;
 
 /****************************************************************************
  * <b>Title</b>: UserAction.java
@@ -72,16 +75,50 @@ public class UserAction extends UserBaseWidget {
 	 */
 	@Override
 	public void build(ActionRequest req) throws ActionException {
+		// Assign the core user data
 		MTSUserVO user = new MTSUserVO(req);
-		if (req.getBooleanParameter("isAuthor")) {
-			try {
-				updateAuthor(user);
-				setModuleData(user);
-			} catch(Exception e) {
-				setModuleData(user, 1, e.getLocalizedMessage());
-				log.error("unable to save author info", e);
+		if (! StringUtil.isEmpty(req.getParameter("value")))
+			user.setImagePath(req.getParameter("value"));
+		
+		// Get the columns to be updated and store the user info
+		String[] cols = null;
+		try {
+			if (req.getBooleanParameter("isAuthor")) {
+				cols = getAuthorUserColumns();
+			} else {
+				cols = getCoreUserColumns();
+				saveUser((SiteVO) req.getAttribute(Constants.SITE_DATA), user, true);
 			}
+
+			updateUser(user, cols);
+			setModuleData(user);
+		} catch(Exception e) {
+			setModuleData(user, 1, e.getLocalizedMessage());
+			log.error("unable to save author info", e);
 		}
+	}
+	
+	/**
+	 * Returns the list of columns to be updated 
+	 * @return
+	 */
+	private String[] getAuthorUserColumns() {
+		return new String[]{ 
+			"user_id", "img_path", "twitter_txt", "facebook_txt", "update_dt",
+			"linkedin_txt", "yrs_experience_no", "cv_desc"
+		};
+	}
+	
+	/**
+	 * List of columns to be updated when saving the core user info
+	 * @return
+	 */
+	private String[] getCoreUserColumns() {
+		return new String[]{ 
+			"user_id", "profile_id", "first_nm", "last_nm", 
+			"email_address_txt", "phone_number_txt", "locale_txt", "update_dt",
+			"active_flg", "role_id", "pro_title_nm","company_nm", "sec_user_id"
+		};
 	}
 	
 	/**
@@ -89,12 +126,7 @@ public class UserAction extends UserBaseWidget {
 	 * @param user
 	 * @throws DatabaseException
 	 */
-	public void updateAuthor(MTSUserVO user) throws DatabaseException {
-		String[] cols = new String[]{ 
-			"user_id", "img_path", "twitter_txt", "facebook_txt", 
-			"linkedin_txt", "yrs_experience_no", "cv_desc"
-		};
-		
+	public void updateUser(MTSUserVO user, String[] cols) throws DatabaseException {
 		DBProcessor db = new DBProcessor(getDBConnection(), getCustomSchema());
 		try {
 			db.update(user, Arrays.asList(cols));
@@ -111,23 +143,47 @@ public class UserAction extends UserBaseWidget {
 	public void retrieve(ActionRequest req) throws ActionException {
 		
 		BSTableControlVO bst = new BSTableControlVO(req, MTSUserVO.class);
-		setModuleData(getAllUsers(bst));
+		setModuleData(getAllUsers(bst, req.getParameter("roleId")));
 	}
 	
 	/**
 	 * 
 	 * @return
 	 */
-	public GridDataVO<MTSUserVO> getAllUsers(BSTableControlVO bst) {
-		StringBuilder sql = new StringBuilder(128);
-		sql.append(DBUtil.SELECT_FROM_STAR).append(getCustomSchema()).append("mts_user a ");
-		sql.append(DBUtil.INNER_JOIN).append("role b on a.role_id = b.role_id ");
-		sql.append("where 1=1 ");
-		sql.append("order by last_nm, first_nm ");
-		log.debug(sql.length() + "|" + sql);
-		
+	public GridDataVO<MTSUserVO> getAllUsers(BSTableControlVO bst, String roleId) {
 		// Add the params
 		List<Object> vals = new ArrayList<>();
+		
+		StringBuilder sql = new StringBuilder(448);
+		sql.append("select a.*, c.role_nm, b.profile_role_id, d.authentication_id from ");
+		sql.append(getCustomSchema()).append("mts_user a ");
+		sql.append(DBUtil.INNER_JOIN).append("profile_role b ");
+		sql.append("on a.profile_id = b.profile_id and site_id = 'MTS_2' ");
+		sql.append(DBUtil.INNER_JOIN).append("role c ");
+		sql.append("on b.role_id = c.role_id ");
+		sql.append(DBUtil.INNER_JOIN).append("profile d ");
+		sql.append("on a.profile_id = d.profile_id ");
+		sql.append("where 1=1 ");
+		
+		// Filter by Roles
+		if (! StringUtil.isEmpty(roleId)) {
+			sql.append("and a.role_id = ? ");
+			vals.add(roleId);
+		}
+		
+		// Filter by the search box
+		if (bst.hasSearch()) {
+			sql.append("and (lower(a.last_nm) like ? or lower(a.first_nm) like ? ");
+			sql.append("or lower(a.email_address_txt) like ?) ");
+			vals.add(bst.getLikeSearch());
+			vals.add(bst.getLikeSearch());
+			vals.add(bst.getLikeSearch());
+		}
+		
+		sql.append(bst.getSQLOrderBy("a.last_nm", "asc"));
+		log.debug(sql.length() + "|" + sql + "|" + bst.getLikeSearch());
+		
+		// Query
 		DBProcessor db = new DBProcessor(getDBConnection());
 		return db.executeSQLWithCount(sql.toString(), vals, new MTSUserVO(), bst);
 	}
