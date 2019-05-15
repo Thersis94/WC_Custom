fd00f6bec73f9bf83d90f7bc242a96803ab5690e
Ryan <ryan@siliconmtn.com>
2019-05-14 22:21:25 -0600

    feat(marathon-pr-24-wslaportal): add report

--- src/com/wsla/action/admin/LanguageBundleMapAction.java
+++ src/com/wsla/action/admin/LanguageBundleMapAction.java
@@ -1,5 +1,8 @@
 package com.wsla.action.admin;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -9,7 +12,10 @@ import com.siliconmtn.action.ActionException;
 import com.siliconmtn.action.ActionInitVO;
 import com.siliconmtn.action.ActionRequest;
 import com.siliconmtn.db.orm.DBProcessor;
+import com.siliconmtn.exception.InvalidDataException;
 import com.siliconmtn.resource.ResourceBundleDataVO;
+import com.siliconmtn.resource.ResourceBundleKeyVO;
+import com.siliconmtn.util.StringUtil;
 // WC Lib
 import com.smt.sitebuilder.action.SBActionAdapter;
 import com.smt.sitebuilder.admin.action.ResourceBundleManagerAction;
@@ -56,15 +62,40 @@ public class LanguageBundleMapAction extends SBActionAdapter {
 	 */
 	@Override
 	public void retrieve(ActionRequest req) throws ActionException {
-		putModuleData(getBundleInfo(req.getParameter("keyId")));
+		try {
+			String keyId = getKeyId(req.getParameter("keyCode"));
+			log.debug("keyId "+ keyId);
+			
+			putModuleData(getBundleInfo(keyId));
+		} catch (Exception e) {
+			log.error("could not load language bundel edits", e);
+			setModuleData(null, 0, e.getLocalizedMessage());
+		}
 	}
 	
+	/**
+	 * Gets the key id from the key code
+	 * @param code
+	 * @return
+	 * @throws SQLException
+	 */
+	public String getKeyId(String code) throws SQLException {
+		String s = "select key_id from resource_bundle_key where key_cd = ?";
+		try (PreparedStatement ps = dbConn.prepareStatement(s)) {
+			ps.setString(1, code);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) return rs.getString(1);
+				else return null;
+			}
+		}
+	}
+ 	
 	/**
 	 * Gets a list of the keys and values for a given bundle key 
 	 * @param statusCode
 	 * @return
 	 */
-	public List<ResourceBundleDataVO> getBundleInfo(String code) {
+	public List<ResourceBundleDataVO> getBundleInfo(String keyId) {
 		StringBuilder sql = new StringBuilder(576);
 		sql.append("select l.country_cd, l.language_cd, l.variant_cd, ");
 		sql.append("b.resource_bundle_data_id, l.key_id, value_txt, resource_bundle_id ");
@@ -73,14 +104,20 @@ public class LanguageBundleMapAction extends SBActionAdapter {
 		sql.append(buildCase(0, "language_cd")).append(",");
 		sql.append(buildCase(1, "country_cd")).append(",");
 		sql.append(buildCase(2, "variant_cd"));
-		sql.append("FROM generate_series(0,").append(WSLALocales.values().length - 1);
+		sql.append("FROM generate_series(0,").append(WSLALocales.getBaseLocales().length - 1);
 		sql.append(") x ").append(") as l ");
 		sql.append("left outer join resource_bundle_data b ");
-		sql.append("on l.key_id = b.key_id and l.country_cd = b.country_cd ");
-		log.debug(sql.length() + "|" + sql);
+		sql.append("on l.key_id = b.key_id and l.country_cd = '' and l.language_cd = b.language_cd ");
 		
 		DBProcessor db = new DBProcessor(getDBConnection());
-		return db.executeSelect(sql.toString(), Arrays.asList(code), new ResourceBundleDataVO(), "language_cd");
+		db.setGenerateExecutedSQL(log.isDebugEnabled());
+		List<ResourceBundleDataVO> results = db.executeSelect(sql.toString(), Arrays.asList(keyId), new ResourceBundleDataVO(), "language_cd");
+		log.debug("############ length " + WSLALocales.getBaseLocales().length + " size " + results.size());  
+		if (WSLALocales.getBaseLocales().length != results.size())	{
+			
+		}
+				
+		return results;
 	}
 
 	/**
@@ -91,13 +128,13 @@ public class LanguageBundleMapAction extends SBActionAdapter {
 	 * @return
 	 */
 	private String buildCase(int type, String label) {
-		WSLALocales[] locales = WSLALocales.values();
-		
+		WSLALocales[] locales = WSLALocales.getBaseLocales();
+
 		StringBuilder cs = new StringBuilder(40);
 		cs.append("case ");
-		for (int i=0; i < locales.length; i++) {
-			Locale locale = new LocaleWrapper(locales[i]).getLocale(); 
-			
+		for (int i=0; i < locales.length ; i++) {
+			WSLALocales localVar = locales[i];
+			Locale locale = new LocaleWrapper(localVar).getLocale(); 
 			String value = "";
 			switch(type) {
 				case 0 :
@@ -126,7 +163,23 @@ public class LanguageBundleMapAction extends SBActionAdapter {
 	public void build(ActionRequest req) throws ActionException {
 		ResourceBundleDataVO dvo = new ResourceBundleDataVO(req);
 		try {
+
 			ResourceBundleManagerAction rbma = new ResourceBundleManagerAction(dbConn, attributes);
+			
+			ResourceBundleKeyVO key = rbma.getKeyByKeyCode(req.getParameter("keyCode"));
+			if(StringUtil.isEmpty(key.getKeyId())) {
+				log.debug("No key exists for this change adding one");
+				
+				String keyCode = StringUtil.checkVal(req.getParameter("keyCode"));
+				String bundleId = StringUtil.checkVal(req.getParameter("resourceBundleId"));
+				String description = StringUtil.checkVal(req.getParameter("description"));
+				//no key exists save a key first
+				//get the new key id and save it to the data bundle
+				dvo.setKeyId(rbma.addNewKey(keyCode, bundleId, description));
+			}else {
+				log.debug("using existing key " + key);
+				dvo.setKeyId(key.getKeyId());
+			}
 			rbma.saveBundleKeyData(dvo);
 		} catch(Exception e) {
 			log.error("Unable to save bundle", e);
@@ -134,4 +187,3 @@ public class LanguageBundleMapAction extends SBActionAdapter {
 		}
 	}
 }
-
