6854595738d324c206b1cff01a89d8d942dce583
James Camire <james@siliconmtn.com>
2019-07-02 09:18:35 -0600

    fix(Json Info Desk): Added the sftp capability to the job

--- src/com/mts/scheduler/job/ContentFeedJob.java
+++ src/com/mts/scheduler/job/ContentFeedJob.java
@@ -1,10 +1,9 @@
 package com.mts.scheduler.job;
 
 // JDK 1.8.x
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -27,6 +26,11 @@ import com.siliconmtn.util.Convert;
 // WC Libs
 import com.smt.sitebuilder.scheduler.AbstractSMTJob;
 
+// Ganymed v3
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.SFTPv3Client;
+import ch.ethz.ssh2.SFTPv3FileHandle;
+
 /****************************************************************************
  * <b>Title</b>: ContentFeedJob.java
  * <b>Project</b>: WC_Custom
@@ -42,10 +46,6 @@ import com.smt.sitebuilder.scheduler.AbstractSMTJob;
  ****************************************************************************/
 
 public class ContentFeedJob extends AbstractSMTJob {
-	/**
-	 * Base URL for links and images
-	 */
-	private static final String BASE_URL = "https://www.innovationinmedtech.com/";
 	
 	// Members
 	private Map<String, Object> attributes;
@@ -87,15 +87,26 @@ public class ContentFeedJob extends AbstractSMTJob {
 	 * @throws IOException 
 	 */
 	public void processDocuments() throws IOException {
+		// Get the data fields
 		String fileLoc = (String)attributes.get("FEED_RECPT");
 		String feedTitle = (String)attributes.get("FEED_TITLE");
 		String feedDesc = (String)attributes.get("FEED_DESC");
+		String host = (String)attributes.get("SFTP_HOST");
+		String user = (String)attributes.get("SFTP_USER");
+		String pwd = (String)attributes.get("SFTP_PASSWORD");
+		String baseUrl = (String)attributes.get("BASE_URL");
 		
-		ContentFeedVO docs = getArticles(feedTitle, feedDesc);
+		// Append the dates to this
+		Date d = new Date();
+		String pattern = "yyyyMMdd";
+		SimpleDateFormat sdf =new SimpleDateFormat(pattern);
+		fileLoc += sdf.format(d) + ".json";
+		
+		ContentFeedVO docs = getArticles(feedTitle, feedDesc, baseUrl);
 		String json = convertArticlesJson(docs);
 		
 		// Save document
-		saveFile(json, fileLoc);
+		saveFile(json, fileLoc, host, user, pwd);
 	}
 	
 	/**
@@ -104,20 +115,35 @@ public class ContentFeedJob extends AbstractSMTJob {
 	 * @param fileLoc
 	 * @throws IOException
 	 */
-	protected void saveFile(String json, String fileLoc) throws IOException {
-		File file = new File (fileLoc);
-		try (FileWriter fw = new FileWriter(file)) {
-			try (BufferedWriter out = new BufferedWriter(fw)) {
-				out.write(json);
-			}
+	protected void saveFile(String json, String fileLoc, String host, String user, String pwd) 
+			throws IOException {
+		// Connect to the server and authenticate
+		Connection conn = new Connection(host);
+		conn.connect();
+		boolean isAuth = conn.authenticateWithPassword(user, pwd);
+		if (! isAuth) throw new IOException("Authentication Failed");
+		
+		// Write the file to the server
+		ByteArrayInputStream bais = new ByteArrayInputStream(json.getBytes());
+		SFTPv3Client sftp = new SFTPv3Client(conn);
+		SFTPv3FileHandle handle = sftp.createFile(fileLoc);
+		byte[] buffer = new byte[1024];
+		long offset=0;
+		int i=0;
+		while ((i = bais.read(buffer)) != -1) {
+			sftp.write(handle, offset, buffer, 0, i);
+			offset += i;
 		}
+		
+		sftp.closeFile(handle);
+		sftp.close();
 	}
 	
 	/**
 	 * 
 	 * @return
 	 */
-	public ContentFeedVO getArticles(String title, String desc) {
+	public ContentFeedVO getArticles(String title, String desc, String baseUrl) {
 		// Build the SQL
 		String schema = (String)this.attributes.get(Constants.CUSTOM_DB_SCHEMA);
 		StringBuilder sql = new StringBuilder(400);
@@ -140,14 +166,14 @@ public class ContentFeedJob extends AbstractSMTJob {
 		ContentFeedVO feed = new ContentFeedVO();
 		feed.setTitle(title);
 		feed.setDescription(desc);
-		feed.setLink(BASE_URL);
+		feed.setLink(baseUrl);
 		feed.setLastBuildDate(new Date());
 		feed.setLocale("en-US");
 		
 		// Get the articles and update the links
 		DBProcessor db = new DBProcessor(conn);		
 		feed.setItems(db.executeSelect(sql.toString(), vals, new ContentFeedItemVO()));
-		updateRelativeLinks(feed);
+		updateRelativeLinks(feed, baseUrl);
 
 		return feed;
 		
@@ -157,11 +183,11 @@ public class ContentFeedJob extends AbstractSMTJob {
 	 * Updates the relative URLs/links to be fully qualified
 	 * @param feed
 	 */
-	protected void updateRelativeLinks(ContentFeedVO feed) {
+	protected void updateRelativeLinks(ContentFeedVO feed, String baseUrl) {
 		// Update the relative links and image sources to be fully qualified
 		for (ContentFeedItemVO item : feed.getItems()) {
 			HTMLFeedParser parser = new HTMLFeedParser();
-			String html = parser.updateDocumentPaths(BASE_URL, item.getContent());
+			String html = parser.updateDocumentPaths(baseUrl, item.getContent());
 			item.setContent(html);
 		}
 	}
