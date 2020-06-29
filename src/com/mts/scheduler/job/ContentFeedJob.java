5f6bf29fecdebcedea6e35c934e908b5f8b480d2
Thersis <thersis94@gmail.com>
2020-06-29 16:56:13 -0600

    refactor: 💡 Revert database connection to original values
    Revert database connection to original values in the setDBConnection
    method of the ContentFeedJob class

--- src/com/mts/scheduler/job/ContentFeedJob.java
+++ src/com/mts/scheduler/job/ContentFeedJob.java
@@ -105,7 +105,7 @@ public class ContentFeedJob extends AbstractSMTJob {
 	private void setDBConnection() throws DatabaseException, InvalidDataException {
 		DatabaseConnection dc = new DatabaseConnection();
 		dc.setDriverClass("org.postgresql.Driver");
-		dc.setUrl("jdbc:postgresql://dev-common-sb-db.aws.siliconmtn.com:5432/wc_dev_sb?defaultRowFetchSize=25&amp;prepareThreshold=3&amp;reWriteBatchedInserts=true");
+		dc.setUrl( "jdbc:postgresql://dev-common-sb-db.aws.siliconmtn.com:5432/wc_dev_sb?defaultRowFetchSize=25&amp;prepareThreshold=3&amp;reWriteBatchedInserts=true");
 		dc.setUserName("ryan_user_sb");
 		dc.setPassword("sqll0gin");
 		conn = dc.getConnection();
